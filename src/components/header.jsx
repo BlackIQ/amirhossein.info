eaea2c773f223f188a63718f45b8ab05b6e1fcae
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-20 23:32:37 +0330

    Added Header

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100 text-white home">
+            <p>
+                Amirhossein Mohammadi
+                <br/>
+                Software developer
+                <br/>
+                <small className="download"/>
+            </p>
+        </div>
+    );
+}
+
+export default Header;

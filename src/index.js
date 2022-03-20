9e56540dad765c117fe2cd74ebd4f151abecfad7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-20 23:32:29 +0330

    Commented script

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import 'mdb-ui-kit/css/mdb.min.css';
 import 'mdb-ui-kit/js/mdb.min.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );

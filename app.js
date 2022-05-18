2b6d10099510b87771581cd2e5cdbd8d27eec7aa
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-18 14:16:26 +0430

    Fix porst

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mdb = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWOR
 mongoose.connect(mdb)
     .then((result) => {
         console.log('Connected');
-        app.listen(3000, 'localhost');
+        app.listen(3000);
     })
     .catch((error) => console.log(error));
 

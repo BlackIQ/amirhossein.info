3709ba04e5fbcabe48857f4aaf092cf8c23e7530
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 16:32:56 +0430

    Imports

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

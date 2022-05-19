9ff9d47a880d9bfc4f732a685b38a4db572c2919
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 16:01:31 +0430

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Test from "./components/test";
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <Router>
+        <Switch>
+          <Route path='/' exact><p>Index</p></Route>
+          <Route path='/messages' exact><p>Messages</p></Route>
+          <Route path='/messages/:id' exact><p>Message</p></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

0282c479820e0e49db663ec626ef7b787d039d29
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 17:07:27 +0430

    Fixes i think

--- client/src/components/messageitem.jsx
+++ client/src/components/messageitem.jsx
@@ -1,8 +1,12 @@
+import { useHistory } from 'react-router-dom';
+
 const MessageItem = (props) => {
     const message = props.message;
 
+    const history = useHistory();
+
     return (
-        <tr class="pointer" onClick={() => window.location.href = `/messages/${message._id}`}>
+        <tr class="pointer" onClick={() => history.push(`/messages/${message._id}`)}>
             <th scope="row">{message.name}</th>
             <td>{message.title}</td>
             <td>{message.createdAt}</td>

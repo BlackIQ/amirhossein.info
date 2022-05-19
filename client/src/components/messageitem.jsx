c0d03a130ba6e99c4c6f36fade44d4f80a014181
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 16:34:44 +0430

    Update id stuff

--- client/src/components/messageitem.jsx
+++ client/src/components/messageitem.jsx
@@ -2,13 +2,11 @@ const MessageItem = (props) => {
     const message = props.message;
 
     return (
-        <tr class="">
+        <tr class="pointer" onClick={() => window.location.href = `/messages/${message._id}`}>
             <th scope="row">{message.name}</th>
             <td>{message.title}</td>
             <td>{message.createdAt}</td>
-            <td className="text-center">
-                <button onClick={() => window.location.href = `/messages/${message._id}`} className="btn btn-sm btn-primary">Open</button>
-            </td>
+            <td>{message.read.toString()}</td>
         </tr>
     );
 }

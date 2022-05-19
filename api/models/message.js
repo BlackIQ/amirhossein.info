d45157f07c564a5b3bb167594a8c6112ce3cdb00
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 10:51:47 +0430

    Move backend there

---
+++ api/models/message.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    read: {
+        type: Boolean,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+const Message = mongoose.model('message', messageSchema);
+
+module.exports = Message;

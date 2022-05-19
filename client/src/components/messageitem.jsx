af322bcfb2cb0faf5186d71bba40f95637cce95a
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 16:32:48 +0430

    Added a sample open

--- client/src/components/messageitem.jsx
+++ client/src/components/messageitem.jsx
@@ -2,11 +2,13 @@ const MessageItem = (props) => {
     const message = props.message;
 
     return (
-        <tr class="pointer">
+        <tr class="">
             <th scope="row">{message.name}</th>
             <td>{message.title}</td>
             <td>{message.createdAt}</td>
-            <td>{message.read}</td>
+            <td className="text-center">
+                <button onClick={() => window.location.href = `/messages/${message._id}`} className="btn btn-sm btn-primary">Open</button>
+            </td>
         </tr>
     );
 }

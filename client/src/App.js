a795df38c23c308f2e580368523377e4addd3ac4
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-19 17:06:32 +0430

    Added Message component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Message from './pages/message';
 
 import Messages from './pages/messages';
 
@@ -9,7 +10,7 @@ function App() {
         <Switch>
           <Route path='/' exact><p>Index</p></Route>
           <Route path='/messages' exact><Messages /></Route>
-          <Route path='/messages/:id' exact><p>Message</p></Route>
+          <Route path='/messages/:id' exact><p><Message /></p></Route>
         </Switch>
       </Router>
     </div>

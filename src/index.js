001c4b17895a00c071c44ea4375c3bdb59d82ddc
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-30 22:37:40 +0430

    Added css

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ import './App.sass';
 import 'mdb-ui-kit/css/mdb.min.css';
 import 'mdb-ui-kit/js/mdb.min.js';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

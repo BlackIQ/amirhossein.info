7cc56162a7ee893f2c3d1a2163e401dbcaa63811
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-30 22:17:11 +0430

    Fixed and etc

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './App.sass';
+
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+root.render(<App />);

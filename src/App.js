1c8717369404d0df3768836b30ab52351ab3425b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-30 21:38:20 +0430

    React

---
+++ src/App.js
@@ -0,0 +1,255 @@
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-expand-lg navbar-dark shadow-0 darkbluenavy-bg">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
+                <a class="navbar-brand boogaloo-font">amirhossein</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    </ul>
+                    <div class="d-flex align-items-center">
+                        <a href="#" class="nav-link text-white">Projects</a>
+                        <a href="https://wa.me/989014784362" target="_blank" class="nav-link"><button class="btn btn-light shadow-0">Hire me</button></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid pt-3 px-3 main">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card rounded-0 shadow-0 mt-2 mb-3">
+                        <div class="card-body intro">
+                            <div class="container">
+                                <div class="row justify-content-start align-items-center">
+                                    <div class="col-md-4">
+                                        <div class="m-1">
+                                            <img src="https://avatars.githubusercontent.com/u/55284339?v=4" class="rounded-circle w-100" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="m-1">
+                                            <h1 class="boogaloo-font">Amirhossein Mohammadi</h1>
+                                            <p><small>Software developer at </small><b>Narbon technologies</b></p>
+                                            <p>
+                                                <span><i class="fa fa-building"></i> Narbon information technologies</span>
+                                                <br/>
+                                                <span><i class="fa fa-map-marker"></i> Turkey, Izmir</span>
+                                                <br/>
+                                                <span><i class="fa fa-birthday-cake"></i> Nov 20 2003</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card rounded-0 shadow-0 mb-3">
+                        <div class="card-header rounded-0">
+                            About me
+                        </div>
+                        <div class="card-body">
+                            <span>Software Developer talented at translating customer requirements into testable engineering plans. Maintains exceptional development quality from conception through distribution. Works alongside clients and colleagues through all stages of development to produce exceptional final products.</span>
+                        </div>
+                    </div>
+                    <div class="card rounded-0 shadow-0 mb-3">
+                        <div class="card-header rounded-0">
+                            Experience
+                        </div>
+                        <div class="card-body">
+                            <div class="job">
+                                <h3 class="font-weight-bold darkbluenavy-fore">Software developer</h3>
+                                <div class="d-flex justify-content-start align-items-end">
+                                    <h4 class="font-weight-bold">Narbon information technologies,</h4>
+                                    &nbsp;
+                                    <h5>Iran, Tehran</h5>
+                                </div>
+                                <h6>Nov 2021, PRESENT</h6>
+                                <ul>
+                                    <li>Revised, modularized and updated old code bases to modern development standards, reducing operating costs and improving functionality.</li>
+                                    <li>Developed software for both desktop and mobile operating systems.</li>
+                                    <li>Collaborated on stages of systems development lifecycle from requirement gathering to production releases.</li>
+                                    <li>Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.</li>
+                                </ul>
+                            </div>
+                            <hr/>
+                            <div class="job">
+                                <h3 class="font-weight-bold darkbluenavy-fore">Back-End developer</h3>
+                                <div class="d-flex justify-content-start align-items-end">
+                                    <h4 class="font-weight-bold">Narbon,</h4>
+                                    &nbsp;
+                                    <h5>Iran, Tehran</h5>
+                                </div>
+                                <h6>Apr 2019, Nov 2021</h6>
+                                <ul>
+                                    <li>Planned website development, converting mockups into usable web presence with HTML, JavaScript, AJAX and JSON coding.</li>
+                                    <li>Provided front-end website development using WordPress and other editing software.</li>
+                                    <li>Coded websites using HTML, CSS, JavaScript and jQuery languages.</li>
+                                    <li>Conducted testing and review of website design for responsiveness, clarity and effectiveness.</li>
+                                    <li>Built and styled new mobile-friendly websites, transitioning legacy presentations to simultaneous easyto-use versions.</li>
+                                    <li>Oversaw back-end development using PHP to maintain website integrity, security and efficiency.</li>
+                                    <li>Pulled from PHP, SQL, JavaScript and other back-end library knowledge to bolster programming resources.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card rounded-0 shadow-0 mb-3">
+                        <div class="card-header rounded-0">
+                            Languages
+                        </div>
+                        <div class="card-body">
+                            <ul>
+                                <li>
+                                    Persian
+                                    <span class="float-end">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </span>
+                                </li>
+                                <li>
+                                    English
+                                    <span class="float-end">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-o"></i>
+                                    </span>
+                                </li>
+                                <li>
+                                    German
+                                    <span class="float-end">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card rounded-0 shadow-0 mb-3">
+                        <div class="card-header rounded-0">
+                            Skills
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr class="darkbluenavy-td">
+                                            <td colspan="4" class="font-weight-bold">Front-End development</td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>ReactJs</li></td>
+                                            <td><li>Material Design</li></td>
+                                            <td><li>SASS</li></td>
+                                            <td><li>Responsive</li></td>
+                                        </tr>
+                                        <tr class="darkbluenavy-td">
+                                            <td colspan="4" class="font-weight-bold">Back-End development</td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>NodeJs</li></td>
+                                            <td><li>ExpressJs</li></td>
+                                            <td><li>Laravel</li></td>
+                                            <td><li>Flask</li></td>
+                                        </tr>
+                                        <tr class="darkbluenavy-td">
+                                            <td colspan="4" class="font-weight-bold">Database</td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>Firebase Firestore</li></td>
+                                            <td><li>PostgresSQL</li></td>
+                                            <td><li>MongoDB</li></td>
+                                            <td><li>MariaDB</li></td>
+                                        </tr>
+                                        <tr class="darkbluenavy-td">
+                                            <td colspan="4" class="font-weight-bold">Languages</td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>Php</li></td>
+                                            <td><li>Python</li></td>
+                                            <td><li>Php</li></td>
+                                            <td><li>Dart</li></td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>C</li></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr class="darkbluenavy-td">
+                                            <td colspan="4" class="font-weight-bold">Others</td>
+                                        </tr>
+                                        <tr>
+                                            <td><li>APIs</li></td>
+                                            <td><li>Documentation</li></td>
+                                            <td><li>Git</li></td>
+                                            <td><li>Linux</li></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card rounded-0 shadow-0 mt-2 mb-3">
+                        <div class="card-header rounded-0">
+                            Send me a message
+                        </div>
+                        <div class="card-body">
+                            <p>Send your messages directly to my email with using this form. Or you can send it with composing an email in your email panel.</p>
+                            <form>
+                                <label class="form-label" for="subject">Subject</label>
+                                <input class="form-control" id="subject" placeholder="Subject" name="subject" />
+                                <br/>
+                                <label class="form-label" for="from">From</label>
+                                <input class="form-control" id="from" placeholder="From" name="from" />
+                                <br/>
+                                <label class="form-label" for="message">Message</label>
+                                <textarea class="form-control" id="message" placeholder="Message" name="message" rows="5"></textarea>
+                                <br/>
+                                <button class="btn btn-lg btn-dark w-100 shadow-0 darkbluenavy-bg">Send message</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="card rounded-0 shadow-0 mt-2 mb-3">
+                        <div class="card-header rounded-0">
+                            Contact me online
+                        </div>
+                        <div class="card-body">
+                            <p>Most of the time I am online in Telegram and Twitter. So i will be glad if we have intraction togather.</p>
+                            <div class="d-flex justify-content-between">
+                                <button type="button" class="btn btn-lg btn-primary btn-floating shadow-0 bg-gradient"><i class="fab fa-telegram-plane"></i></button>
+                                <button type="button" class="btn btn-lg btn-dark btn-floating shadow-0 bg-gradient"><i class="fab fa-github"></i></button>
+                                <button type="button" class="btn btn-lg btn-primary btn-floating shadow-0 bg-gradient"><i class="fab fa-linkedin-in"></i></button>
+                                <button type="button" class="btn btn-lg btn-success btn-floating shadow-0 bg-gradient"><i class="fab fa-whatsapp"></i></button>
+                                <button type="button" class="btn btn-lg btn-info btn-floating shadow-0 bg-gradient"><i class="fab fa-twitter"></i></button>
+                                <button type="button" class="btn btn-lg btn-primary btn-floating shadow-0 bg-gradient"><i class="fab fa-facebook-f"></i></button>
+                            </div>
+                            <hr/>
+                            <h4>Also, remember my email and phone</h4>
+                            <span>
+                                <i class="fas fa-envelope darkbluenavy-fore"></i>
+                                <a class="text-reset" href="mailto:amir@yahoo.com">amir@yahoo.com</a>
+                            </span>
+                            <br/>
+                            <span>
+                                <i class="fas fa-mobile-alt darkbluenavy-fore"></i>
+                                <a class="text-reset" href="tel:+989014784362">+98 901 4784 362</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;

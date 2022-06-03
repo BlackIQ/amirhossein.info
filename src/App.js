75dabe7d964be503ca1e18b55eb71d85771012b5
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-03 23:25:20 +0430

    Added MBE Trading

--- src/App.js
+++ src/App.js
@@ -254,41 +254,24 @@ function App() {
                     </div>
                     <div className="card rounded-0 shadow-0 mb-3">
                         <div className="card-header rounded-0">
-                            Languages
+                            Projects
                         </div>
                         <div className="card-body">
-                            <ul>
-                                <li>
-                                    Persian
-                                    <span className="float-end">
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                    </span>
-                                </li>
-                                <li>
-                                    English
-                                    <span className="float-end">
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star-o"></i>
-                                    </span>
-                                </li>
-                                <li>
-                                    German
-                                    <span className="float-end">
-                                        <i className="fa fa-star"></i>
-                                        <i className="fa fa-star-o"></i>
-                                        <i className="fa fa-star-o"></i>
-                                        <i className="fa fa-star-o"></i>
-                                        <i className="fa fa-star-o"></i>
-                                    </span>
-                                </li>
-                            </ul>
+                            <div className='project'>
+                                <div className="d-flex justify-content-start align-items-center">
+                                    <h3 className='font-weight-bold darkbluenavy-fore'><a href='https://mbrtrading.com' className='text-reset' target='_blank'>MBR TRADING</a></h3>
+                                    &nbsp;
+                                    <h6>Dubai, UAE</h6>
+                                </div>
+                                <div className="d-flex justify-content-start align-items-center">
+                                    <i>#Wordpress, #Elementor</i>
+                                    &nbsp;
+                                    |
+                                    &nbsp;
+                                    <small>2020, 2021</small>
+                                </div>
+                            </div>
+                            <hr/>
                         </div>
                     </div>
                 </div>

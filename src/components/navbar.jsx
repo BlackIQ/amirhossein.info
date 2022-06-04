2136541de3c6308312f0ec89f9ad4f627e0ef027
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-04 14:44:38 +0430

    Add hire me notify

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,27 @@
+import { toast } from 'react-toastify';
+
 const Navbar = () => {
+    const hiremeNotify = () => toast.warning('🤦🏻‍♂️ Send your message in send message form!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark shadow-0 darkbluenavy-bg">
             <div className="container">
                 <button className="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i className="fas fa-bars"></i></button>
-                <a className="navbar-brand boogaloo-font">amirhossein</a>
+                <a href='/' className="navbar-brand boogaloo-font">amirhossein</a>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     </ul>
                     <div className="d-flex align-items-center">
                         <a href="#projects" className="nav-link text-white">Projects</a>
-                        <a href="https://wa.me/989014784362" target="_blank" className="nav-link"><button className="btn btn-light shadow-0">Hire me</button></a>
+                        <span onClick={hiremeNotify} className="nav-link"><button className="btn btn-light shadow-0">Hire me</button></span>
                     </div>
                 </div>
             </div>

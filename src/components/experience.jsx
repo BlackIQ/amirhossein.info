9d638eb2e182eae595c7672c03351b334926e722
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-04 15:25:31 +0430

    New component, exprience

---
+++ src/components/experience.jsx
@@ -0,0 +1,47 @@
+const Experience = () => {
+    return (
+        <div className="card rounded-0 shadow-0 mb-3">
+            <div className="card-header rounded-0">
+                Experience
+            </div>
+            <div className="card-body">
+                <div className="job">
+                    <h3 className="font-weight-bold darkbluenavy-fore">Software developer</h3>
+                    <div className="d-flex justify-content-start align-items-end">
+                        <h4 className="font-weight-bold">Narbon information technologies.</h4>
+                        &nbsp;
+                        <h5>Iran, Tehran</h5>
+                    </div>
+                    <h6>Nov 2021, PRESENT</h6>
+                    <ul>
+                        <li>Revised, modularized and updated old code bases to modern development standards, reducing operating costs and improving functionality.</li>
+                        <li>Developed software for both desktop and mobile operating systems.</li>
+                        <li>Collaborated on stages of systems development lifecycle from requirement gathering to production releases.</li>
+                        <li>Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.</li>
+                    </ul>
+                </div>
+                <hr/>
+                <div className="job">
+                    <h3 className="font-weight-bold darkbluenavy-fore">Back-End developer</h3>
+                    <div className="d-flex justify-content-start align-items-end">
+                        <h4 className="font-weight-bold">Narbon.</h4>
+                        &nbsp;
+                        <h5>Iran, Tehran</h5>
+                    </div>
+                    <h6>Apr 2019, Nov 2021</h6>
+                    <ul>
+                        <li>Planned website development, converting mockups into usable web presence with HTML, JavaScript, AJAX and JSON coding.</li>
+                        <li>Provided front-end website development using WordPress and other editing software.</li>
+                        <li>Coded websites using HTML, CSS, JavaScript and jQuery languages.</li>
+                        <li>Conducted testing and review of website design for responsiveness, clarity and effectiveness.</li>
+                        <li>Built and styled new mobile-friendly websites, transitioning legacy presentations to simultaneous easyto-use versions.</li>
+                        <li>Oversaw back-end development using PHP to maintain website integrity, security and efficiency.</li>
+                        <li>Pulled from PHP, SQL, JavaScript and other back-end library knowledge to bolster programming resources.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Experience;

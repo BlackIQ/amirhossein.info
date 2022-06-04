bfb2471190a40d76df3bb6be2aac8e109feb0196
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-04 15:25:52 +0430

    New component, skills

---
+++ src/components/skills.jsx
@@ -0,0 +1,85 @@
+const Skills = () => {
+    return (
+        <div className="card rounded-0 shadow-0 mb-3">
+            <div className="card-header rounded-0">
+                Skills
+            </div>
+            <div className="card-body">
+                <div className="table-responsive">
+                    <table className="table table-bordered">
+                        <tbody>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Front-End development</td>
+                            </tr>
+                            <tr>
+                                <td><li>ReactJs</li></td>
+                                <td><li>Material Design</li></td>
+                                <td><li>SASS</li></td>
+                                <td><li>Responsive</li></td>
+                            </tr>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Back-End development</td>
+                            </tr>
+                            <tr>
+                                <td><li>NodeJs</li></td>
+                                <td><li>ExpressJs</li></td>
+                                <td><li>Laravel</li></td>
+                                <td><li>Flask</li></td>
+                            </tr>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Mobile development</td>
+                            </tr>
+                            <tr>
+                                <td><li>Dart</li></td>
+                                <td><li>Flutter</li></td>
+                                <td><li>iOS development</li></td>
+                                <td><li>Android development</li></td>
+                            </tr>
+                            <tr>
+                                <td><li>Firebase</li></td>
+                                <td><li>MongoDB</li></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Database</td>
+                            </tr>
+                            <tr>
+                                <td><li>Firebase Firestore</li></td>
+                                <td><li>PostgresSQL</li></td>
+                                <td><li>MongoDB</li></td>
+                                <td><li>MariaDB</li></td>
+                            </tr>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Languages</td>
+                            </tr>
+                            <tr>
+                                <td><li>Php</li></td>
+                                <td><li>Python</li></td>
+                                <td><li>Php</li></td>
+                                <td><li>Dart</li></td>
+                            </tr>
+                            <tr>
+                                <td><li>C</li></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr className="darkbluenavy-td">
+                                <td colspan="4" className="font-weight-bold">Others</td>
+                            </tr>
+                            <tr>
+                                <td><li>APIs</li></td>
+                                <td><li>Documentation</li></td>
+                                <td><li>Git</li></td>
+                                <td><li>Linux</li></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;

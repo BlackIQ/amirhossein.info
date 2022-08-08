8a2c48dfb4ddd8eee4fab81c4b26262f03ff1280
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 15:53:37 +0430

    Fix ex stuff

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,56 +6,43 @@ const Experience = () => {
             </div>
             <div className="card-body">
                 <div className="job">
-                    <h3 className="font-weight-bold darkbluenavy-fore">Software developer</h3>
+                    <h3 className="font-weight-bold darkbluenavy-fore">CEO / Software developer</h3>
                     <div className="d-flex justify-content-start align-items-end">
-                        <h4 className="font-weight-bold">Narbon information technologies.</h4>
+                        <h5 className="font-weight-bold">TFAsoft</h5>
                         &nbsp;
-                        <h5>Iran, Tehran</h5>
+                        <h6>Turkey, Izmir</h6>
                     </div>
-                    <h6>Nov 2021, PRESENT</h6>
-                    <ul>
-                        <li>Revised, modularized and updated old code bases to modern development standards, reducing operating costs and improving functionality.</li>
-                        <li>Developed software for both desktop and mobile operating systems.</li>
-                        <li>Collaborated on stages of systems development lifecycle from requirement gathering to production releases.</li>
-                        <li>Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.</li>
-                    </ul>
+                    <h6>May 2022, PRESENT</h6>
                 </div>
                 <hr/>
                 <div className="job">
                     <h3 className="font-weight-bold darkbluenavy-fore">Software developer</h3>
                     <div className="d-flex justify-content-start align-items-end">
-                        <h4 className="font-weight-bold">Merh Charitable.</h4>
+                        <h5 className="font-weight-bold">Narbon technologies.</h5>
                         &nbsp;
-                        <h5>Iran, Tehran</h5>
+                        <h6>Iran, Tehran</h6>
                     </div>
-                    <h6>Jun 2022, PRESENT</h6>
-                    <ul>
-                        <li>Designed intuitive graphical user interfaces using knowledge of serial communications and database design to improve user experience.</li>
-                        <li>Validated personal coding and third-party code to verify compliance with internal standards and technical requirements.</li>
-                        <li>Worked with the CEO to synchronize web presence with brand identity and logo.</li>
-                        <li>Collaborated with graphic design personnel to integrate images as basic site components.</li>
-                        <li>Pulled from PHP, SQL, JavaScript and other back-end library knowledge to bolster programming resources.</li>
-                        <li>Translated verbal customer requirements into written use cases and testable development stories.</li>
-                    </ul>
+                    <h6>Nov 2021, PRESENT</h6>
+                </div>
+                <hr/>
+                <div className="job">
+                    <h3 className="font-weight-bold darkbluenavy-fore">Wordpress developer</h3>
+                    <div className="d-flex justify-content-start align-items-end">
+                        <h5 className="font-weight-bold">Merh Charitable.</h5>
+                        &nbsp;
+                        <h6>Iran, Tehran</h6>
+                    </div>
+                    <h6>Jun 2022, Aug 2022</h6>
                 </div>
                 <hr/>
                 <div className="job">
                     <h3 className="font-weight-bold darkbluenavy-fore">Back-End developer</h3>
                     <div className="d-flex justify-content-start align-items-end">
-                        <h4 className="font-weight-bold">Narbon.</h4>
+                        <h5 className="font-weight-bold">Narbon.</h5>
                         &nbsp;
-                        <h5>Iran, Tehran</h5>
+                        <h6>Iran, Tehran</h6>
                     </div>
                     <h6>Apr 2019, Nov 2021</h6>
-                    <ul>
-                        <li>Planned website development, converting mockups into usable web presence with HTML, JavaScript, AJAX and JSON coding.</li>
-                        <li>Provided front-end website development using WordPress and other editing software.</li>
-                        <li>Coded websites using HTML, CSS, JavaScript and jQuery languages.</li>
-                        <li>Conducted testing and review of website design for responsiveness, clarity and effectiveness.</li>
-                        <li>Built and styled new mobile-friendly websites, transitioning legacy presentations to simultaneous easyto-use versions.</li>
-                        <li>Oversaw back-end development using PHP to maintain website integrity, security and efficiency.</li>
-                        <li>Pulled from PHP, SQL, JavaScript and other back-end library knowledge to bolster programming resources.</li>
-                    </ul>
                 </div>
             </div>
         </div>

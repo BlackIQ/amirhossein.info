3c66453dc89d4576d21b26d1c3d53723691dd957
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-16 20:51:12 +0430

    Add about card

---
+++ src/cards/about.card.jsx
@@ -0,0 +1,13 @@
+import {
+    Typography,
+} from "@mui/material";
+
+const AboutMeCard = () => {
+    return (
+        <Typography>
+            Software Developer talented at translating customer requirements into testable engineering plans. Maintains exceptional development quality from conception through distribution. Works alongside clients and colleagues through all stages of development to produce exceptional final products.
+        </Typography>
+    );
+}
+
+export default AboutMeCard;

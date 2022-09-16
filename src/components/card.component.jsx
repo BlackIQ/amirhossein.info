fe0591e528e3a1e0fb80496cf6e3853704317f1f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-16 19:58:18 +0430

    Create dcard component for both about and normal

---
+++ src/components/card.component.jsx
@@ -0,0 +1,66 @@
+import {
+    Typography,
+    Avatar,
+    Card,
+    CardHeader,
+    CardContent,
+} from "@mui/material";
+
+export const AppCard = (props) => {
+    const { children, icon, title, subtitle } = props;
+
+    return (
+        <Card
+            variant="outlined"
+            sx={{
+                borderRadius: 1,
+                borderColor: "primary.main",
+                mb: 2,
+            }}
+        >
+            <CardHeader
+                title={
+                    <Typography fontWeight="bold">
+                        { title }
+                    </Typography>
+                }
+                subheader={
+                    <Typography>
+                        { subtitle }
+                    </Typography>
+                }
+                avatar={
+                    <Avatar sx={{ bgcolor: "primary.main" }}>
+                        { icon }
+                    </Avatar>
+                }
+                sx={{
+                    bgcolor: "primary.main",
+                    color: "white",
+                }}
+            />
+            <CardContent>
+                { children }
+            </CardContent>
+        </Card>
+    );
+}
+
+export const AboutCard = (props) => {
+    const { children } = props;
+
+    return (
+        <Card
+            variant="outlined"
+            sx={{
+                borderRadius: 1,
+                borderColor: "primary.main",
+                mb: 2,
+            }}
+        >
+            <CardContent>
+                { children }
+            </CardContent>
+        </Card>
+    );
+}

cc32311df8643548159c39b37ca30cc134e84a97
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-16 19:58:32 +0430

    Remove some stuff

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './App.sass';
-
-import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.min.js';
-
-import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+    <App />
+);

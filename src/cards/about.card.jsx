85c873bc8f42a8f6a5539ac8cdec74a08877d063
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-19 20:48:47 +0430

    Add body1 variant

--- src/cards/about.card.jsx
+++ src/cards/about.card.jsx
@@ -4,7 +4,9 @@ import {
 
 const AboutMeCard = () => {
     return (
-        <Typography>
+        <Typography
+            variant="body1"
+        >
             Software Developer talented at translating customer requirements into testable engineering plans. Maintains exceptional development quality from conception through distribution. Works alongside clients and colleagues through all stages of development to produce exceptional final products.
         </Typography>
     );

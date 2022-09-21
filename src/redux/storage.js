0e64715ffd69f6050199b3c225770d5fd2269ba7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-21 21:14:57 +0430

    Add storre

---
+++ src/redux/storage.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+
+import { saveState, loadState } from "./localstorage";
+import { allReducers } from "./reducers";
+
+const presentedState = loadState();
+
+export const store = createStore(
+    allReducers,
+    presentedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.subscribe(() => saveState({
+  session: store.getState().session,
+  user: store.getState().user,
+}))

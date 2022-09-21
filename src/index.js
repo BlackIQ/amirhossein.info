07907cb4908d9f520d684165e57e22f716f16b21
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-21 21:14:14 +0430

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
+import { Provider } from "react-redux";
+import { store } from "./redux/storage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );

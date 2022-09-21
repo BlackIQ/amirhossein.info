80147f9ac8163554568eba9e880e7092ea90f951
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-21 21:14:06 +0430

    Add navbar items like login, logout and panel

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 import {
     AppBar,
@@ -11,9 +13,24 @@ import {
 
 import MakeSnackbar from "./snackbar.component";
 
+import { unsetSession } from "../redux/session/action";
+import { unsetUserToken } from "../redux/user/action";
+
 const Navbar = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const session = useSelector(state => state.session);
+
     const [snackOpen, setSnackOpen] = useState(false);
     const [snackMessage, setSnackMessage] = useState("");
+
+    const logout = () => {
+        dispatch(unsetSession());
+        dispatch(unsetUserToken());
+    }
+
+    const login = () => history.push("/authentication");
 
     const hireMe = () => {
         setSnackMessage("Please leave a message in the send message form 🙏🏻");
@@ -28,8 +45,10 @@ const Navbar = () => {
                         <Typography
                             variant="h5"
                             fontFamily="Boogaloo"
+                            onClick={() => history.push("/")}
                             sx={{
                                 flexGrow: 1,
+                                cursor: "pointer",
                             }}
                         >
                             amirhossein
@@ -44,6 +63,30 @@ const Navbar = () => {
                         >
                             Hire me
                         </Button>
+                        <Button
+                            color="inherit"
+                            variant="text"
+                            onClick={() => session ? logout() : login()}
+                            sx={{
+                                fontWeight: "bold"
+                            }}
+                        >
+                            { session ? "Logout" : "Login" }
+                        </Button>
+                        {
+                            session
+                            &&
+                            <Button
+                                color="inherit"
+                                variant="text"
+                                onClick={() => history.push("/panel")}
+                                sx={{
+                                    fontWeight: "bold"
+                                }}
+                            >
+                                Panel
+                            </Button>
+                        }
                     </Toolbar>
                 </Container>
             </AppBar>

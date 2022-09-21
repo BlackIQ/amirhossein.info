2cdc92624bc09f585f87bdbddb61ec072d22482b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-09-21 21:13:43 +0430

    Add localstorage

---
+++ src/redux/localstorage.js
@@ -0,0 +1,25 @@
+const key = 'amirhossein';
+
+export const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem(key);
+
+        if (serializedState === null) {
+            return undefined;
+        }
+
+        return JSON.parse(serializedState);
+
+    } catch (error) {
+        return undefined;
+    }
+};
+
+export const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(key, serializedState);
+    } catch (error) {
+        // die
+    }
+};

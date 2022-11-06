5c2b8ecb6994cb1e4d2758005f3094a7013665e7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-11-06 15:29:12 +0330

    Combine the components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar/navbar.component";
+import Snackbar from "./snackbar/snackbar.component";
+import Card from "./snackbar/snackbar.component";
+
+export { Navbar, Snackbar, Card };
---
+++ src/components/snackbar/snackbar.component.jsx
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+
+import { Snackbar, Typography, IconButton } from "@mui/material";
+
+import { Close } from "@mui/icons-material";
+
+const MakeSnackbar = (props) => {
+  const { open, close, message } = props;
+
+  const action = (
+    <Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={close}
+      >
+        <Close fontSize="small" />
+      </IconButton>
+    </Fragment>
+  );
+
+  const showMessage = (message) => (
+    <Typography variant="body1" fontWeight="bold">
+      {message}
+    </Typography>
+  );
+
+  return (
+    <Snackbar
+      open={open}
+      onClose={close}
+      action={action}
+      autoHideDuration={3000}
+      message={showMessage(message)}
+    />
+  );
+};
+
+export default MakeSnackbar;

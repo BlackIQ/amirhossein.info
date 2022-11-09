9fc371174510a6bf0716c1d50cd2c0940aa58243
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-11-09 14:16:59 +0330

    Remove view services

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,4 @@
-import * as ViewServices from "./view/view.services";
 import * as MessageServices from "./message/message.services";
 import * as AuthServices from "./auth/auth.services";
 
-export { ViewServices, MessageServices, AuthServices };
+export { MessageServices, AuthServices };

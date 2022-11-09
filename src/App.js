37d9f60673053db55c7ff825146072a352fdf008
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-11-09 14:17:10 +0330

    Router stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Router from "./router";
 import { Layout } from "./layout";
+import { theme } from "./theme";
+
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const mode = useSelector((state) => state.theme);
+
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <ThemeProvider theme={theme(mode)}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Layout>
+          <Router />
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import { BrowserRouter as Router } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
-import { ThemeProvider, CssBaseline } from "@mui/material";
 
 import { store } from "./redux/storage";
-import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ReduxProvider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <App />
-      </Router>
-    </ThemeProvider>
+    <App />
   </ReduxProvider>
 );

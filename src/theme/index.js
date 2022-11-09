b2d7213053506232e9dc67510e9131813f2a3107
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-11-09 14:11:38 +0330

    Read theme with arg

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,18 +1,17 @@
 import { createTheme, colors } from "@mui/material";
 
-const mode = "dark";
-
-export const theme = createTheme({
-  palette: {
-    mode,
-    background: {
-      default: mode === "light" ? colors.blueGrey[100] : colors.grey[900],
+export const theme = (mode) =>
+  createTheme({
+    palette: {
+      mode,
+      background: {
+        default: mode === "light" ? colors.blueGrey[100] : colors.grey[900],
+      },
+      primary: {
+        main: mode === "light" ? colors.blueGrey[800] : colors.blueGrey[500],
+      },
     },
-    primary: {
-      main: mode === "light" ? colors.blueGrey[800] : colors.blueGrey[500],
+    typography: {
+      fontFamily: "Quicksand, Boogaloo, Meow Script, Patrick Hand",
     },
-  },
-  typography: {
-    fontFamily: "Quicksand, Boogaloo, Meow Script, Patrick Hand",
-  },
-});
+  });

37646d218fd0867778ec5cf36f4b19d1bbdd2b66
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-31 01:21:13 +0330

    Using API for socials

--- src/cards/social.card.jsx
+++ src/cards/social.card.jsx
@@ -3,49 +3,50 @@ import {
   List,
   ListItem,
   ListItemText,
-  ListItemIcon,
   ListItemButton,
   Typography,
 } from "@mui/material";
 
-import { GitHub, LinkedIn, Email } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 
-const medias = [
-  {
-    icon: <GitHub />,
-    text: "GitHub",
-    path: "https://github.com/BlackIQ",
-  },
-  {
-    icon: <LinkedIn />,
-    text: "LinkedIn",
-    path: "https://linkedin.com/in/amirhosseinmohammadi",
-  },
-  {
-    icon: <Email />,
-    text: "Email",
-    path: "mailto:hi@amirhossein.info",
-  },
-];
+import { API } from "../api";
+import { Loading } from "../components";
 
 const SocialCard = () => {
+  const [medias, setMedias] = useState([]);
+
+  const getSocials = async () => {
+    try {
+      const data = await API.get("socials");
+
+      setMedias(data.data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getSocials();
+  }, []);
+
   return (
     <Box>
       <Typography variant="body1" gutterButton>
         Let's have a connection in social medias!
       </Typography>
-      <List>
-        {medias.map((media) => (
-          <ListItem key={media.text} disablePadding divider>
-            <ListItemButton onClick={() => window.location.replace(media.path)}>
-              <ListItemIcon sx={{ color: "primary.main" }}>
-                {media.icon}
-              </ListItemIcon>
-              <ListItemText primary={media.text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      {medias.length > 0 ? (
+        <List>
+          {medias.map((media) => (
+            <ListItem key={media._id} disablePadding divider>
+              <ListItemButton onClick={() => window.open(media.url)}>
+                <ListItemText primary={media.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      ) : (
+        <Loading />
+      )}
     </Box>
   );
 };

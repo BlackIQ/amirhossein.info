637d5bbecc2f3e0817c8d056e29e019ae6f8bc07
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-03-31 11:05:08 +0330

    Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { Box, Grid, Toolbar } from "@mui/material";
+import { Box, Grid, Toolbar, Typography } from "@mui/material";
 
 import {
   Person,
@@ -14,65 +14,108 @@ import {
 
 import { Snackbar, Card } from "@/components";
 import * as Cards from "@/cards";
+import { API } from "@/api";
 
-const mainCards = [
-  {
-    component: <Cards.AboutMeCard />,
-    title: "About me",
-    subtitle: "Read about this guy",
-    icon: <Person sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-  {
-    component: <Cards.ExperiencesCard />,
-    title: "Experiences",
-    subtitle: "Companies I worked",
-    icon: <BusinessCenter sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-  {
-    component: <Cards.APICard />,
-    title: "API",
-    subtitle: "Services and APIs introduction",
-    icon: <Settings sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-];
+export const getServerSideProps = async () => {
+  const data = {};
 
-const sideCards = [
-  {
-    component: <Cards.MessageCard />,
-    title: "Send a message",
-    subtitle: "Talk to me!",
-    icon: <Email sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-  {
-    component: <Cards.SocialCard />,
-    title: "Social media",
-    subtitle: "Let's contact in social media",
-    icon: <Tag sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-  {
-    component: <Cards.SkillsCard />,
-    title: "Skills",
-    subtitle: "Technologies or stuff I can work with",
-    icon: <Handyman sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-  {
-    component: <Cards.DownloadCard />,
-    title: "Download resume",
-    subtitle: "Download my resume in PDF",
-    icon: <Download sx={{ color: "white", fontSize: 30 }} />,
-    hide: false,
-  },
-];
+  try {
+    const resumes = await API.get("resumes");
 
-export default function Home() {
+    data["resumes"] = { data: resumes.data };
+  } catch (error) {
+    data["resumes"] = { error: error.response.data };
+  }
+
+  try {
+    const skills = await API.get("skills");
+
+    data["skills"] = { data: skills.data };
+  } catch (error) {
+    data["skills"] = { error: error.response.data };
+  }
+
+  try {
+    const socials = await API.get("socials");
+
+    data["socials"] = { data: socials.data };
+  } catch (error) {
+    data["socials"] = { error: error.response.data };
+  }
+
+  try {
+    const experiences = await API.get("experiences");
+
+    data["experiences"] = { data: experiences.data };
+  } catch (error) {
+    data["experiences"] = { error: error.response.data };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ data }) {
   const [snackOpen, setSnackOpen] = useState(false);
   const [snackMessage, setSnackMessage] = useState("");
+
+  const mainCards = [
+    {
+      component: <Cards.AboutMeCard />,
+      title: "About me",
+      subtitle: "Read about this guy",
+      icon: <Person sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+    {
+      component: <Cards.ExperiencesCard experiences={data.experiences} />,
+      title: "Experiences",
+      subtitle: "Companies I worked",
+      icon: <BusinessCenter sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+    {
+      component: <Cards.APICard />,
+      title: "API",
+      subtitle: "Services and APIs introduction",
+      icon: <Settings sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+  ];
+
+  const sideCards = [
+    {
+      component: <Cards.MessageCard />,
+      title: "Send a message",
+      subtitle: "Talk to me!",
+      icon: <Email sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+    {
+      component: <Cards.SocialCard socials={data.socials} />,
+      title: "Social media",
+      subtitle: "Let's contact in social media",
+      icon: <Tag sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+    {
+      component: <Cards.SkillsCard skills={data.skills} />,
+      title: "Skills",
+      subtitle: "Technologies or stuff I can work with",
+      icon: <Handyman sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+    {
+      component: <Cards.ResumeCard resumes={data.resumes} />,
+      title: "Download resume",
+      subtitle: "Download my resume in PDF",
+      icon: <Download sx={{ color: "white", fontSize: 30 }} />,
+      hide: false,
+    },
+  ];
 
   useEffect(() => {
     setSnackMessage("Welcome 🎉");
@@ -83,7 +126,7 @@ export default function Home() {
     <Box sx={{ mx: 2, mt: 2 }}>
       <Toolbar />
       <Grid spacing={2} container>
-        <Grid md={8} item>
+        <Grid md={8} width="100%" item>
           <Cards.MainCard />
           {mainCards.map(
             (card) =>
@@ -99,7 +142,7 @@ export default function Home() {
               )
           )}
         </Grid>
-        <Grid md={4} item>
+        <Grid md={4} width="100%" item>
           {sideCards.map(
             (card) =>
               !card.hide && (

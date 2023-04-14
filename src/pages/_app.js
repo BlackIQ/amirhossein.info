f1f570c86c8dc9bf8d914a45ad0c74d8f434e0fd
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-14 17:37:07 +0330

    _app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,13 @@
 import { Provider as ReduxProvider } from "react-redux";
 
 import { store } from "@/redux/storage";
-import { AppLayout } from "@/layout";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <ReduxProvider store={store}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <Component {...pageProps} />
     </ReduxProvider>
   );
 }

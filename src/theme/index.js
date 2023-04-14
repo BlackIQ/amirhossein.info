3ba1c9e02cf61a45abbcd1470e428afbcc11d507
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-14 17:35:14 +0330

    Handling multi themes

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,6 +1,6 @@
 import { createTheme, colors } from "@mui/material";
 
-export const theme = (mode) =>
+export const appTheme = (mode) =>
   createTheme({
     palette: {
       mode,
@@ -9,6 +9,23 @@ export const theme = (mode) =>
       },
       primary: {
         main: mode === "light" ? colors.blueGrey[800] : colors.blueGrey[500],
+      },
+    },
+    typography: {
+      fontFamily:
+        "Vazirmatn, Noto Serif Georgian, Boogaloo, Meow Script, Patrick Hand",
+    },
+  });
+
+export const adminTheme = (mode) =>
+  createTheme({
+    palette: {
+      mode,
+      background: {
+        default: mode === "light" ? colors.teal[50] : colors.grey[900],
+      },
+      primary: {
+        main: mode === "light" ? colors.teal[800] : colors.teal[500],
       },
     },
     typography: {

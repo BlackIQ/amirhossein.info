d8619fdfd36f91ffac0e917fb9cb109581d4ae18
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-10-06 20:39:54 +0330

    Tst

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,14 @@
-import { Provider as ReduxProvider } from "react-redux";
+// import { Provider as ReduxProvider } from "react-redux";
 
-import { store } from "@/redux/storage";
+// import { store } from "@/redux/storage";
 
 import "@/styles/globals.css";
 import "@/styles/globals.sass";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ReduxProvider store={store}>
-      <Component {...pageProps} />
-    </ReduxProvider>
+    // <ReduxProvider store={store}>
+    <Component {...pageProps} />
+    // </ReduxProvider>
   );
 }

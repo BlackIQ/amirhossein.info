4eee147d134b434b0a1a0636d0855aa1bb99ecbf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-10-06 19:25:45 +0330

    More updates

--- src/layout/app/app.layout.js
+++ src/layout/app/app.layout.js
@@ -1,9 +1,6 @@
-import Head from "next/head";
-
 import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import { useSelector } from "react-redux";
 
-import { Navbar } from "@/components";
 import { appTheme } from "@/theme";
 
 export const AppLayout = ({ children }) => {
@@ -11,15 +8,9 @@ export const AppLayout = ({ children }) => {
 
   return (
     <>
-      <Head>
-        <title>Amirhossein Mohammadi</title>
-      </Head>
       <ThemeProvider theme={appTheme(mode)}>
         <CssBaseline />
-        <Box>
-          <Navbar />
-          {children}
-        </Box>
+        <Box>{children}</Box>
       </ThemeProvider>
     </>
   );

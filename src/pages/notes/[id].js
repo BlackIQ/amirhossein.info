a5d3a4f82b4d9cccb2a6b4876e07c2282c6f1806
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-10-23 03:31:26 +0330

    SSR

--- src/pages/notes/[id].js
+++ src/pages/notes/[id].js
@@ -16,8 +16,8 @@ import { API } from "@/api";
 
 import Head from "next/head";
 import { Navbar, Loading } from "@/components";
-import { useEffect, useState } from "react";
-import { VolunteerActivism } from "@mui/icons-material";
+// import { useEffect, useState } from "react";
+// import { VolunteerActivism } from "@mui/icons-material";
 
 export const getServerSidePaths = async () => {
   const paths = [];
@@ -35,121 +35,121 @@ export const getServerSidePaths = async () => {
 };
 
 export const getServerSideProps = async ({ params }) => {
-  return {
-    props: {
-      params,
-    },
-  };
+  try {
+    const note = await API.get(`notes/${params.id}`);
+
+    return { props: { data: note.data } };
+  } catch (error) {
+    return { props: { error } };
+  }
+
+  // return {
+  //   props: {
+  //     params,
+  //   },
+  // };
 };
 
-export default function Notes({ params }) {
+export default function Notes({ params, data, error }) {
   const getDateInFormat = (ts) => {
     const d = new Date(ts);
 
     return `${d.getFullYear()}/${d.getMonth()}/${d.getDay()} ${d.getHours()}:${d.getMinutes()}`;
   };
 
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  const getData = async () => {
-    try {
-      const note = await API.get(`notes/${params.id}`);
+  // const getData = async () => {
+  //   try {
+  //     const note = await API.get(`notes/${params.id}`);
 
-      setData(note.data);
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+  //     setData(note.data);
+  //   } catch (error) {
+  //     alert(error.message);
+  //   }
+  // };
 
-  const clap = async () => {
-    try {
-      await API.patch(`notes/clap/${params.id}`);
+  // const clap = async () => {
+  //   try {
+  //     await API.patch(`notes/clap/${data._id}`);
 
-      getData();
-    } catch (error) {
-      alert(error.response.data.message);
-    }
-  };
+  //     getData();
+  //   } catch (error) {
+  //     alert(error.response.data.message);
+  //   }
+  // };
+
+  if (error) {
+    return <Box>{error.message}</Box>;
+  }
 
   return (
     <>
-      {data ? (
-        <Head>
-          <title>{data.title}</title>
-          <meta name="description" content={data.details} />
-          <meta property="og:title" content={data.title} />
-          <meta property="og:description" content={data.details} />
-          <meta property="og:image" content={data.thumbnail} />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content={data.title} />
-          <meta property="twitter:description" content={data.details} />
-          <meta property="twitter:image" content={data.thumbnail} />
-        </Head>
-      ) : (
-        <Head>
-          <title>Loading . . .</title>
-        </Head>
-      )}
+      <Head>
+        <title>{data.title}</title>
+        <meta name="description" content={data.details} />
+        <meta property="og:title" content={data.title} />
+        <meta property="og:description" content={data.details} />
+        <meta property="og:image" content={data.thumbnail} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={data.title} />
+        <meta property="twitter:description" content={data.details} />
+        <meta property="twitter:image" content={data.thumbnail} />
+      </Head>
 
-      {!data ? (
-        <Box>
-          <Loading />
-        </Box>
-      ) : (
-        <AppLayout>
-          <Navbar />
-          <Toolbar />
-          <Container sx={{ py: 3 }}>
-            <Box className="markdown">
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {data.content}
-              </ReactMarkdown>
-            </Box>
+      <AppLayout>
+        <Navbar />
+        <Toolbar />
+        <Container sx={{ py: 3 }}>
+          <Box className="markdown">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {data.content}
+            </ReactMarkdown>
+          </Box>
 
-            <Divider sx={{ borderColor: "primary", my: 4 }}>
-              Thanks for reading 🙏
-            </Divider>
+          <Divider sx={{ borderColor: "primary", my: 4 }}>
+            Thanks for reading 🙏
+          </Divider>
+
+          <Box
+            sx={{
+              p: 4,
+              borderRadius: 3,
+              border: "solid 1px",
+              borderColor: "primary.main",
+            }}
+          >
+            <Typography variant="body1" gutterBottom>
+              In this note you read about <b>{data.details}</b>
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Posted at {getDateInFormat(data.createdAt)}
+            </Typography>
+
+            <br />
 
             <Box
               sx={{
-                p: 4,
-                borderRadius: 3,
-                border: "solid 1px",
-                borderColor: "primary.main",
+                display: "flex",
+                height: "100%",
+                justifyContent: "space-between",
+                alignItems: "center",
               }}
             >
-              <Typography variant="body1" gutterBottom>
-                In this note you read about <b>{data.details}</b>
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Posted at {getDateInFormat(data.createdAt)}
-              </Typography>
-
-              <br />
-
-              <Box
-                sx={{
-                  display: "flex",
-                  height: "100%",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <Typography variant="body1">Amirhossein Mohammadi</Typography>
-                <Button
+              <Typography variant="body1">Amirhossein Mohammadi</Typography>
+              {/* <Button
                   startIcon={<VolunteerActivism color="error" onClick={clap} />}
                 >
                   {data.clap}
-                </Button>
-              </Box>
+                </Button> */}
             </Box>
-          </Container>
-        </AppLayout>
-      )}
+          </Box>
+        </Container>
+      </AppLayout>
     </>
   );
 }

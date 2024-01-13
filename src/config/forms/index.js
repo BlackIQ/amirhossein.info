824be959d9af4d7cd026d0dd9d14774d203f463f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-01-13 08:24:39 +0330

    Add messaege form

--- src/config/forms/index.js
+++ src/config/forms/index.js
@@ -17,6 +17,29 @@ const forms = [
     },
   },
   {
+    name: "message",
+    fields: {
+      name: {
+        type: "text",
+        label: "Name",
+        placeholder: "Enter your name",
+        secure: false,
+      },
+      email: {
+        type: "text",
+        label: "Email",
+        placeholder: "Enter your email",
+        secure: false,
+      },
+      message: {
+        type: "textarea",
+        label: "Message",
+        placeholder: "Can not wait for your message",
+        secure: false,
+      },
+    },
+  },
+  {
     name: "socials",
     fields: {
       label: {

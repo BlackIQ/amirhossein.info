9fbadcabd541610149f29b005087970818c68e21
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-05-30 22:35:51 +0330

    Better

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,35 +1,19 @@
 import { createTheme, colors } from "@mui/material";
 
-export const appTheme = (mode) =>
-  createTheme({
-    palette: {
-      mode,
-      background: {
-        default: mode === "light" ? colors.blueGrey[50] : colors.grey[900],
-      },
-      primary: {
-        main: mode === "light" ? colors.blueGrey[800] : colors.blueGrey[500],
-      },
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: colors.blueGrey[50],
     },
-    typography: {
-      fontFamily:
-        "Vazirmatn, Noto Serif Georgian, Boogaloo, Meow Script, Patrick Hand",
+    primary: {
+      main: colors.blueGrey[800],
     },
-  });
+  },
+  typography: {
+    fontFamily:
+      "Vazirmatn, Noto Serif Georgian, Boogaloo, Meow Script, Patrick Hand",
+  },
+});
 
-export const adminTheme = (mode) =>
-  createTheme({
-    palette: {
-      mode,
-      background: {
-        default: mode === "light" ? colors.teal[50] : colors.grey[900],
-      },
-      primary: {
-        main: mode === "light" ? colors.teal[800] : colors.teal[500],
-      },
-    },
-    typography: {
-      fontFamily:
-        "Vazirmatn, Noto Serif Georgian, Boogaloo, Meow Script, Patrick Hand",
-    },
-  });
+export { theme };

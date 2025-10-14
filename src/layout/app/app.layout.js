60bf596fd4ce6f79edbfd0232d326fc663bc1f76
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-10-14 08:17:34 +0330

    LAYOUT: Using getTheme method

--- src/layout/app/app.layout.js
+++ src/layout/app/app.layout.js
@@ -3,8 +3,10 @@ import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import { theme } from "@/theme";
 
 export const AppLayout = ({ children }) => {
+  const themeMode = "light";
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme(themeMode)}>
       <CssBaseline />
       <Box>{children}</Box>
     </ThemeProvider>

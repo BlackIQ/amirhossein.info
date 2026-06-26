80f97fecb48702c01d4e51d7535904700714cb7f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-26 19:06:31 +0330

    Testing

--- backend/app/connections/mongo/mongo.connection.js
+++ backend/app/connections/mongo/mongo.connection.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 import {MONGO_CONNECTION_STRING} from "$app/config/index.js";
 
-console.log(MONGO_CONNECTION_STRING);
-
 export const mongodb = mongoose.createConnection(MONGO_CONNECTION_STRING, (error) => {
     if (error) {
         console.log(error);

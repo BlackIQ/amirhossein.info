fda329ad4547ebec04f288cff81a435cb9c0064f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:35:59 +0330

    Module migration: social

---
+++ backend/src/modules/social/social.controller.ts
@@ -0,0 +1,72 @@
+import Social from "@src/modules/social/social.model.js";
+
+export const CREATE = async (req, res) => {
+  const data = req.body;
+
+  try {
+    const social = await Social.create(data);
+
+    return res.status(200).send(social);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const ALL = async (req, res) => {
+  try {
+    const socials = await Social.find({ show: true }).sort({ priority: 1 });
+
+    return res.status(200).send(socials);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const SINGLE = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const social = await Social.findById(id);
+
+    if (!social) {
+      return res.status(404).send({ message: "Social did not found" });
+    }
+
+    return res.status(200).send(social);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const DELETE = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const social = await Social.findByIdAndDelete(id);
+
+    if (!social) {
+      return res.status(404).send({ message: "Social did not found" });
+    }
+
+    return res.status(200).send({ message: "Social deleted" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const UPDATE = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  try {
+    const social = await Social.findByIdAndUpdate(id, data);
+
+    if (!social) {
+      return res.status(404).send({ message: "Social did not found" });
+    }
+
+    return res.status(200).send({ message: "Social updated" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};

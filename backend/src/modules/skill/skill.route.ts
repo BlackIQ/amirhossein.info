dc8d7a579f11a07ff23769613f82bdafb8af7c96
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:36:15 +0330

    Module migration: skill

---
+++ backend/src/modules/skill/skill.route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+  CREATE,
+  ALL,
+  SINGLE,
+  DELETE,
+  UPDATE,
+} from "@src/modules/skill/skill.controller.js";
+import { key } from "@src/middlewares/index.js";
+
+const router = express.Router();
+
+router.post("/", key, CREATE);
+router.get("/", ALL);
+router.get("/:id", SINGLE);
+router.delete("/:id", key, DELETE);
+router.patch("/:id", key, UPDATE);
+
+export default router;

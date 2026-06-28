7c7b17dd671042660919ff7d38f2d0c9d041cf47
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:34:11 +0330

    Update MongoDB connection

--- backend/src/connections/mongo/mongo.connection.ts
+++ backend/src/connections/mongo/mongo.connection.ts
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
+import { MONGO_CONNECTION_STRING } from "@src/config/index.js";
 
-import { MONGO_CONNECTION_STRING } from "$app/config/index.js";
+export const mongodb = mongoose.createConnection(MONGO_CONNECTION_STRING);
 
-export const mongodb = mongoose.createConnection(
-  MONGO_CONNECTION_STRING,
-  (error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Connected to mongodb.");
-    }
-  },
-);
+mongodb.on("connected", () => {
+  console.log("Connected to mongo.");
+});
+
+mongodb.on("error", (error) => {
+  console.log("Mongo connection error:", error);
+});

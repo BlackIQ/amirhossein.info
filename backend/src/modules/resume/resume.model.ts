b02a465e6faaf743f59c202d9663b1dc1d037a8e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:35:52 +0330

    Module migration: resume

---
+++ backend/src/modules/resume/resume.model.ts
@@ -0,0 +1,32 @@
+import { mongodb } from "@src/connections/mongo/mongo.connection.js";
+
+import mongoose from "mongoose";
+
+const mongooseSchema = mongoose.Schema;
+
+const schemaModel = {
+  priority: {
+    type: Number,
+    default: null,
+  },
+  label: {
+    type: String,
+    default: null,
+  },
+  value: {
+    type: String,
+    default: null,
+  },
+  url: {
+    type: String,
+    default: null,
+  },
+  show: {
+    type: Boolean,
+    default: true,
+  },
+};
+
+const schema = new mongooseSchema(schemaModel, { timestamps: true });
+
+export default mongodb.model("Resume", schema);

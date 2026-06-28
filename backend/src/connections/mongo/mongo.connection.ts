8110bf5526ce99e52175dc31c7649f4f17a8bf43
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 23:06:55 +0330

    Single connection is ok

--- backend/src/connections/mongo/mongo.connection.ts
+++ backend/src/connections/mongo/mongo.connection.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import { MONGO_CONNECTION_STRING } from "@src/config/index.js";
 
-export const mongodb = mongoose.createConnection(MONGO_CONNECTION_STRING);
+export const mongodb = mongoose.connect(MONGO_CONNECTION_STRING);
 
-mongodb.on("connected", () => {
+mongodb.then(() => {
   console.log("Connected to mongo.");
 });
 
-mongodb.on("error", (error) => {
+mongodb.catch((error) => {
   console.log("Mongo connection error:", error);
 });

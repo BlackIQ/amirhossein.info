939f7ac3f545f0989e4263f32630dec48f080323
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:33:38 +0330

    Delete models

--- backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-import Social from "$app/models/social/social.model.js";
-import Skill from "$app/models/skill/skill.model.js";
-import Experience from "$app/models/experience/experience.model.js";
-import Resume from "$app/models/resume/resume.model.js";
-import Message from "$app/models/message/message.model.js";
-import Note from "$app/models/note/note.model.js";
-
-export { Social, Skill, Experience, Resume, Message, Note };

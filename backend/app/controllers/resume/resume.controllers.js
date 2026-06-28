742c7af38a13fc7a4e934dadb835160e68393e41
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 11:29:37 +0330

    A a show in document to show only items that is availiable

--- backend/app/controllers/resume/resume.controllers.js
+++ backend/app/controllers/resume/resume.controllers.js
@@ -1,72 +1,72 @@
-import {Resume} from "$app/models/index.js";
+import { Resume } from "$app/models/index.js";
 
 export const CREATE = async (req, res) => {
-    const data = req.body;
+  const data = req.body;
 
-    try {
-        const resume = await Resume.create(data);
+  try {
+    const resume = await Resume.create(data);
 
-        return res.status(200).send(resume);
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-    }
+    return res.status(200).send(resume);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };
 
 export const ALL = async (req, res) => {
-    try {
-        const resumes = await Resume.find().sort({priority: 1});
+  try {
+    const resumes = await Resume.find({ show: true }).sort({ priority: 1 });
 
-        return res.status(200).send(resumes);
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-    }
+    return res.status(200).send(resumes);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };
 
 export const SINGLE = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    try {
-        const resume = await Resume.findById(id);
+  try {
+    const resume = await Resume.findById(id);
 
-        if (!resume) {
-            return res.status(404).send({message: "Resume did not found"});
-        }
-
-        return res.status(200).send(resume);
-    } catch (error) {
-        return res.status(500).send({message: error.message});
+    if (!resume) {
+      return res.status(404).send({ message: "Resume did not found" });
     }
+
+    return res.status(200).send(resume);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };
 
 export const DELETE = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    try {
-        const resume = await Resume.findByIdAndDelete(id);
+  try {
+    const resume = await Resume.findByIdAndDelete(id);
 
-        if (!resume) {
-            return res.status(404).send({message: "Resume did not found"});
-        }
-
-        return res.status(200).send({message: "Resume deleted"});
-    } catch (error) {
-        return res.status(500).send({message: error.message});
+    if (!resume) {
+      return res.status(404).send({ message: "Resume did not found" });
     }
+
+    return res.status(200).send({ message: "Resume deleted" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };
 
 export const UPDATE = async (req, res) => {
-    const {id} = req.params;
-    const data = req.body;
+  const { id } = req.params;
+  const data = req.body;
 
-    try {
-        const resume = await Resume.findByIdAndUpdate(id, data);
+  try {
+    const resume = await Resume.findByIdAndUpdate(id, data);
 
-        if (!resume) {
-            return res.status(404).send({message: "Resume did not found"});
-        }
-
-        return res.status(200).send({message: "Resume updated"});
-    } catch (error) {
-        return res.status(500).send({message: error.message});
+    if (!resume) {
+      return res.status(404).send({ message: "Resume did not found" });
     }
+
+    return res.status(200).send({ message: "Resume updated" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };

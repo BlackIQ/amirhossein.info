7ac549c1a78937f520604bc84d75c18364121550
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:33:19 +0330

    Delete controllers

--- backend/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import * as Social from "$app/controllers/social/social.controllers.js";
-import * as Skill from "$app/controllers/skill/skill.controllers.js";
-import * as Experience from "$app/controllers/experience/experience.controllers.js";
-import * as Resume from "$app/controllers/resume/resume.controllers.js";
-import * as Message from "$app/controllers/message/message.controllers.js";
-import * as Base from "$app/controllers/base/base.controllers.js";
-import * as Note from "$app/controllers/note/note.controllers.js";
-
-export { Social, Skill, Experience, Resume, Message, Base, Note };

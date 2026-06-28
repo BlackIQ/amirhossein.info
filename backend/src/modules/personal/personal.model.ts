342b98724a2db3de4ff69d0e0bd18dfa773db17d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-28 14:36:06 +0330

    Module migration: personal

---
+++ backend/src/modules/personal/personal.model.ts
@@ -0,0 +1,20 @@
+import { mongodb } from "@src/connections/mongo/mongo.connection.js";
+
+import mongoose from "mongoose";
+
+const mongooseSchema = mongoose.Schema;
+
+const schemaModel = {
+  label: {
+    type: String,
+    default: null,
+  },
+  value: {
+    type: String,
+    default: null,
+  },
+};
+
+const schema = new mongooseSchema(schemaModel, { timestamps: true });
+
+export default mongodb.model("Info", schema);

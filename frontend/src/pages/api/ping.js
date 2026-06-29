78dcdb293af856401211aa785e0ac7667d8b19f2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-29 18:07:36 +0330

    Delete app stuff

--- frontend/src/pages/api/ping.js
+++
@@ -1,7 +0,0 @@
-export default function ping(req, res) {
-  console.log("==========================");
-  console.log("Here it is");
-  console.log("==========================");
-
-  return res.status(200).send({ message: "Ok" });
-}

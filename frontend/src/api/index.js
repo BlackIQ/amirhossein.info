7ef3895b9fde0ca31093c0376b98ea3acb78778c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-29 15:26:19 +0330

    NextAPI

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env["NEXT_PUBLIC_API_URL"],
+  baseURL: process.env["NEXT_PUBLIC_API_URL"],
+});
+
+export const NextAPI = axios.create({
+  baseURL: "/api",
 });

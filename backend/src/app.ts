6b6d76bc128886344ec95d5a092293fc11877ba2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-01 11:08:04 +0330

    Move routes to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,14 @@ import cors from "cors";
 import morgan from "morgan";
 
 import { initDB } from "@src/connections/mongo/mongo.connection.js";
-import Routes from "@src/routes/index.js";
+
+import Experience from "@src/modules/experience/experience.route.js";
+import Skill from "@src/modules/skill/skill.route.js";
+import Social from "@src/modules/social/social.route.js";
+import Resume from "@src/modules/resume/resume.route.js";
+import Message from "@src/modules/message/message.route.js";
+import Base from "@src/modules/base/base.route.js";
+import Note from "@src/modules/note/note.route.js";
 
 const app = express();
 
@@ -17,7 +24,14 @@ app.set("json spaces", 2);
 
 app.use(morgan("dev"));
 
-app.use("/api", Routes);
+app.use("/api/experiences", Experience);
+app.use("/api/skills", Skill);
+app.use("/api/socials", Social);
+app.use("/api/resumes", Resume);
+app.use("/api/messages", Message);
+app.use("/api/base", Base);
+app.use("/api/notes", Note);
+
 app.use("*", (req, res) =>
   res.status(404).send({
     url: req.originalUrl,

8d083597ca3af017f43f76ef52501ce1195481ab
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-03 20:04:19 +0330

    Implementing Middleware, API-Key, Handle API Key, Pydantic settings, Other improvments

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@
 from pathlib import Path
 
 # Import FastAPI, Pydantic, SQLAlchemy, SQLAlchemy ORM
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Security
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from sqlalchemy import Boolean, Column, Integer, String, create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
@@ -97,6 +99,14 @@ class NoteModel(Base):
 
 # Create tables
 Base.metadata.create_all(bind=engine)
+
+
+# Settings
+class Settings(BaseSettings):
+    api_key: str = ''
+
+
+settings = Settings()
 
 # FastAPI app
 app = FastAPI(
@@ -209,22 +219,57 @@ def get_db():
         db.close()
 
 
+# Middlewares
+header_schema = APIKeyHeader(name="X-API-KEY", description="API Key in header")
+
+
+# Handle API-Key
+async def handle_apikey(api_key: str = Security(header_schema)):
+    if not api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing API key"
+        )
+
+    if api_key != settings.api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API key"
+        )
+
+
+# ---------- Routes ---------- #
+
+
+# Root
 @app.get("/")
 async def read_root():
     return {"message": "Amirhossein FastAPI backend is running"}
 
 
+# Welcome
+@app.get("/api", dependencies=[Depends(handle_apikey)])
+async def api():
+    return {"message": "Welcome to Amirhossein FastAPI backend"}
+
+
+# Health check route
 @app.get("/api/ping")
 async def ping():
     return {"message": "pong"}
 
 
+# ---------- Experience ---------- #
+
+# Get all Experiences
 @app.get("/api/experiences", response_model=list[ExperienceRead], tags=["Experiences"])
 async def all_experiences(db: Session = Depends(get_db)):
     return db.query(ExperienceModel).all()
 
 
-@app.post("/api/experiences", response_model=ExperienceRead, status_code=status.HTTP_201_CREATED, tags=["Experiences"])
+# Create one Experience
+@app.post("/api/experiences", response_model=ExperienceRead, status_code=status.HTTP_201_CREATED, tags=["Experiences"],
+          dependencies=[Depends(handle_apikey)])
 async def create_experience(experience: Experience, db: Session = Depends(get_db)):
     db_item = ExperienceModel(**experience.model_dump())
     db.add(db_item)
@@ -233,6 +278,7 @@ async def create_experience(experience: Experience, db: Session = Depends(get_db
     return db_item
 
 
+# Get one Experience
 @app.get("/api/experiences/{experience_id}", response_model=ExperienceRead, tags=["Experiences"])
 async def get_experience(experience_id: int, db: Session = Depends(get_db)):
     item = db.get(ExperienceModel, experience_id)
@@ -241,7 +287,9 @@ async def get_experience(experience_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/experiences/{experience_id}", response_model=ExperienceRead, tags=["Experiences"])
+# Update one Experience
+@app.put("/api/experiences/{experience_id}", response_model=ExperienceRead, tags=["Experiences"],
+         dependencies=[Depends(handle_apikey)])
 async def update_experience(experience_id: int, experience: Experience, db: Session = Depends(get_db)):
     item = db.get(ExperienceModel, experience_id)
     if not item:
@@ -255,7 +303,9 @@ async def update_experience(experience_id: int, experience: Experience, db: Sess
     return item
 
 
-@app.delete("/api/experiences/{experience_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Experiences"])
+# Delete one Experience
+@app.delete("/api/experiences/{experience_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Experiences"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_experience(experience_id: int, db: Session = Depends(get_db)):
     item = db.get(ExperienceModel, experience_id)
     if not item:
@@ -265,12 +315,17 @@ async def delete_experience(experience_id: int, db: Session = Depends(get_db)):
     return None
 
 
+# ---------- Skill ---------- #
+
+# Get all Skills
 @app.get("/api/skills", response_model=list[SkillRead], tags=["Skills"])
 async def all_skills(db: Session = Depends(get_db)):
     return db.query(SkillModel).all()
 
 
-@app.post("/api/skills", response_model=SkillRead, status_code=status.HTTP_201_CREATED, tags=["Skills"])
+# Create one Skill
+@app.post("/api/skills", response_model=SkillRead, status_code=status.HTTP_201_CREATED, tags=["Skills"],
+          dependencies=[Depends(handle_apikey)])
 async def create_skill(skill: Skill, db: Session = Depends(get_db)):
     db_item = SkillModel(**skill.model_dump())
     db.add(db_item)
@@ -279,6 +334,7 @@ async def create_skill(skill: Skill, db: Session = Depends(get_db)):
     return db_item
 
 
+# Get one Skill
 @app.get("/api/skills/{skill_id}", response_model=SkillRead, tags=["Skills"])
 async def get_skill(skill_id: int, db: Session = Depends(get_db)):
     item = db.get(SkillModel, skill_id)
@@ -287,7 +343,9 @@ async def get_skill(skill_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/skills/{skill_id}", response_model=SkillRead, tags=["Skills"])
+# Update one Skill
+@app.put("/api/skills/{skill_id}", response_model=SkillRead, tags=["Skills"],
+         dependencies=[Depends(handle_apikey)])
 async def update_skill(skill_id: int, skill: Skill, db: Session = Depends(get_db)):
     item = db.get(SkillModel, skill_id)
     if not item:
@@ -301,7 +359,9 @@ async def update_skill(skill_id: int, skill: Skill, db: Session = Depends(get_db
     return item
 
 
-@app.delete("/api/skills/{skill_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Skills"])
+# Delete one Skill
+@app.delete("/api/skills/{skill_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Skills"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_skill(skill_id: int, db: Session = Depends(get_db)):
     item = db.get(SkillModel, skill_id)
     if not item:
@@ -311,12 +371,17 @@ async def delete_skill(skill_id: int, db: Session = Depends(get_db)):
     return None
 
 
+# ---------- Social ---------- #
+
+# Get all Socials
 @app.get("/api/socials", response_model=list[SocialRead], tags=["Socials"])
 async def all_socials(db: Session = Depends(get_db)):
     return db.query(SocialModel).all()
 
 
-@app.post("/api/socials", response_model=SocialRead, status_code=status.HTTP_201_CREATED, tags=["Socials"])
+# Create one Social
+@app.post("/api/socials", response_model=SocialRead, status_code=status.HTTP_201_CREATED, tags=["Socials"],
+          dependencies=[Depends(handle_apikey)])
 async def create_social(social: Social, db: Session = Depends(get_db)):
     db_item = SocialModel(**social.model_dump())
     db.add(db_item)
@@ -325,6 +390,7 @@ async def create_social(social: Social, db: Session = Depends(get_db)):
     return db_item
 
 
+# Get one Social
 @app.get("/api/socials/{social_id}", response_model=SocialRead, tags=["Socials"])
 async def get_social(social_id: int, db: Session = Depends(get_db)):
     item = db.get(SocialModel, social_id)
@@ -333,7 +399,9 @@ async def get_social(social_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/socials/{social_id}", response_model=SocialRead, tags=["Socials"])
+# Update one Social
+@app.put("/api/socials/{social_id}", response_model=SocialRead, tags=["Socials"],
+         dependencies=[Depends(handle_apikey)])
 async def update_social(social_id: int, social: Social, db: Session = Depends(get_db)):
     item = db.get(SocialModel, social_id)
     if not item:
@@ -347,7 +415,9 @@ async def update_social(social_id: int, social: Social, db: Session = Depends(ge
     return item
 
 
-@app.delete("/api/socials/{social_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Socials"])
+# Delete one Social
+@app.delete("/api/socials/{social_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Socials"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_social(social_id: int, db: Session = Depends(get_db)):
     item = db.get(SocialModel, social_id)
     if not item:
@@ -357,12 +427,17 @@ async def delete_social(social_id: int, db: Session = Depends(get_db)):
     return None
 
 
+# ---------- Resume ---------- #
+
+# Get all Resumes
 @app.get("/api/resumes", response_model=list[ResumeRead], tags=["Resumes"])
 async def all_resumes(db: Session = Depends(get_db)):
     return db.query(ResumeModel).all()
 
 
-@app.post("/api/resumes", response_model=ResumeRead, status_code=status.HTTP_201_CREATED, tags=["Resumes"])
+# Create one Resume
+@app.post("/api/resumes", response_model=ResumeRead, status_code=status.HTTP_201_CREATED, tags=["Resumes"],
+          dependencies=[Depends(handle_apikey)])
 async def create_resume(resume: Resume, db: Session = Depends(get_db)):
     db_item = ResumeModel(**resume.model_dump())
     db.add(db_item)
@@ -371,6 +446,7 @@ async def create_resume(resume: Resume, db: Session = Depends(get_db)):
     return db_item
 
 
+# Get one Resume
 @app.get("/api/resumes/{resume_id}", response_model=ResumeRead, tags=["Resumes"])
 async def get_resume(resume_id: int, db: Session = Depends(get_db)):
     item = db.get(ResumeModel, resume_id)
@@ -379,7 +455,9 @@ async def get_resume(resume_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/resumes/{resume_id}", response_model=ResumeRead, tags=["Resumes"])
+# Update one Resume
+@app.put("/api/resumes/{resume_id}", response_model=ResumeRead, tags=["Resumes"],
+         dependencies=[Depends(handle_apikey)])
 async def update_resume(resume_id: int, resume: Resume, db: Session = Depends(get_db)):
     item = db.get(ResumeModel, resume_id)
     if not item:
@@ -393,7 +471,9 @@ async def update_resume(resume_id: int, resume: Resume, db: Session = Depends(ge
     return item
 
 
-@app.delete("/api/resumes/{resume_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Resumes"])
+# Delete one Resume
+@app.delete("/api/resumes/{resume_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Resumes"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_resume(resume_id: int, db: Session = Depends(get_db)):
     item = db.get(ResumeModel, resume_id)
     if not item:
@@ -403,12 +483,17 @@ async def delete_resume(resume_id: int, db: Session = Depends(get_db)):
     return None
 
 
+# ---------- Message ---------- #
+
+# Get all Messages
 @app.get("/api/messages", response_model=list[MessageRead], tags=["Message"])
 async def all_messages(db: Session = Depends(get_db)):
     return db.query(MessageModel).all()
 
 
-@app.post("/api/messages", response_model=MessageRead, status_code=status.HTTP_201_CREATED, tags=["Message"])
+# Create one Message
+@app.post("/api/messages", response_model=MessageRead, status_code=status.HTTP_201_CREATED, tags=["Message"],
+          dependencies=[Depends(handle_apikey)])
 async def create_message(message: Message, db: Session = Depends(get_db)):
     db_item = MessageModel(**message.model_dump())
     db.add(db_item)
@@ -417,6 +502,7 @@ async def create_message(message: Message, db: Session = Depends(get_db)):
     return db_item
 
 
+# Get one Message
 @app.get("/api/messages/{message_id}", response_model=MessageRead, tags=["Message"])
 async def get_message(message_id: int, db: Session = Depends(get_db)):
     item = db.get(MessageModel, message_id)
@@ -425,7 +511,9 @@ async def get_message(message_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/messages/{message_id}", response_model=MessageRead, tags=["Message"])
+# Update one Message
+@app.put("/api/messages/{message_id}", response_model=MessageRead, tags=["Message"],
+         dependencies=[Depends(handle_apikey)])
 async def update_message(message_id: int, message: Message, db: Session = Depends(get_db)):
     item = db.get(MessageModel, message_id)
     if not item:
@@ -439,7 +527,9 @@ async def update_message(message_id: int, message: Message, db: Session = Depend
     return item
 
 
-@app.delete("/api/messages/{message_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Message"])
+# Delete one Message
+@app.delete("/api/messages/{message_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Message"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_message(message_id: int, db: Session = Depends(get_db)):
     item = db.get(MessageModel, message_id)
     if not item:
@@ -449,12 +539,17 @@ async def delete_message(message_id: int, db: Session = Depends(get_db)):
     return None
 
 
+# ---------- Note ---------- #
+
+# Get all Notes
 @app.get("/api/notes", response_model=list[NoteRead], tags=["Note"])
 async def all_notes(db: Session = Depends(get_db)):
     return db.query(NoteModel).all()
 
 
-@app.post("/api/notes", response_model=NoteRead, status_code=status.HTTP_201_CREATED, tags=["Note"])
+# Create one Note
+@app.post("/api/notes", response_model=NoteRead, status_code=status.HTTP_201_CREATED, tags=["Note"],
+          dependencies=[Depends(handle_apikey)])
 async def create_note(note: Note, db: Session = Depends(get_db)):
     db_item = NoteModel(**note.model_dump())
     db.add(db_item)
@@ -463,6 +558,7 @@ async def create_note(note: Note, db: Session = Depends(get_db)):
     return db_item
 
 
+# Get one Note
 @app.get("/api/notes/{note_id}", response_model=NoteRead, tags=["Note"])
 async def get_note(note_id: int, db: Session = Depends(get_db)):
     item = db.get(NoteModel, note_id)
@@ -471,7 +567,9 @@ async def get_note(note_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/api/notes/{note_id}", response_model=NoteRead, tags=["Note"])
+# Update one Note
+@app.put("/api/notes/{note_id}", response_model=NoteRead, tags=["Note"],
+         dependencies=[Depends(handle_apikey)])
 async def update_note(note_id: int, note: Note, db: Session = Depends(get_db)):
     item = db.get(NoteModel, note_id)
     if not item:
@@ -485,7 +583,9 @@ async def update_note(note_id: int, note: Note, db: Session = Depends(get_db)):
     return item
 
 
-@app.delete("/api/notes/{note_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Note"])
+# Delete one Note
+@app.delete("/api/notes/{note_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Note"],
+            dependencies=[Depends(handle_apikey)])
 async def delete_note(note_id: int, db: Session = Depends(get_db)):
     item = db.get(NoteModel, note_id)
     if not item:

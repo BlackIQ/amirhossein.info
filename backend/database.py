2e803e7a9c8ceb4716aa69a2755feb3b2c701c58
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 12:58:05 +0330

    I have nothing to say

--- backend/database.py
+++ backend/database.py
@@ -13,7 +13,8 @@ from settings import settings
 # DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 # DATABASE_URL = f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DATABASE}?sslmode=require&supa=base-pooler.x"
-DATABASE_URL = f'postgres+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@aws-0-eu-west-1.pooler.supabase.com:6543/{settings.POSTGRES_DATABASE}?sslmode=require&supa=base-pooler.x'
+# DATABASE_URL = f'postgres+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@aws-0-eu-west-1.pooler.supabase.com:6543/{settings.POSTGRES_DATABASE}?sslmode=require&supa=base-pooler.x'
+DATABASE_URL = settings.POSTGRES_URL_NON_POOLING
 
 print(DATABASE_URL)
 

85c5a7ea7e485aea06a481803e7298476dc9dc69
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 12:40:09 +0330

    WIP on Vercel+Supabase

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,8 @@ from settings import settings
 # Path to .db file
 # DB_PATH = Path(__file__).resolve().parent / "app.db"
 # DATABASE_URL = f"sqlite:///{DB_PATH}"
-DATABASE_URL = settings.POSTGRES_URL
+
+DATABASE_URL = f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DATABASE}"
 
 # DB Engine
 engine = create_engine(
--- backend/settings.py
+++ backend/settings.py
@@ -4,9 +4,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Settings
 class Settings(BaseSettings):
+    # Security
     API_KEY: str = ''
+
+    # Local DB
     DATABASE_URL: str = ''
+
+    # SupaBase
     POSTGRES_URL: str = ''
+    POSTGRES_USER: str = ''
+    POSTGRES_PASSWORD: str = ''
+    POSTGRES_HOST: str = ''
+    POSTGRES_DATABASE: str = ''
 
     model_config = SettingsConfigDict(env_file=".env")
 

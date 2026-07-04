4af8ea716f6141d6d1b84e52d889f29c9b6ace64
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 12:22:38 +0330

    Going to test SupaBase on Vercel

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,22 @@
-# Pathlib
-from pathlib import Path
+# PathLib
+# from pathlib import Path
 
 # SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Settings
-# from settings import settings
+from settings import settings
 
 # Path to .db file
-DB_PATH = Path(__file__).resolve().parent / "app.db"
-DATABASE_URL = f"sqlite:///{DB_PATH}"
-# DATABASE_URL = settings.DATABASE_URL
+# DB_PATH = Path(__file__).resolve().parent / "app.db"
+# DATABASE_URL = f"sqlite:///{DB_PATH}"
+DATABASE_URL = settings.DATABASE_URL
 
 # DB Engine
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}  # Fucking SQLite
+    # connect_args={"check_same_thread": False}  # Fucking SQLite
 )
 
 # Session

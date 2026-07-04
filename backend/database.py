67388017b1b7b78dfbd8ae936e9e4d7582352707
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 09:26:51 +0330

    Move database from main.py to database.py

---
+++ backend/database.py
@@ -0,0 +1,19 @@
+# Pathlib
+from pathlib import Path
+
+# SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Path to .db file
+DB_PATH = Path(__file__).resolve().parent / "app.db"
+DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+# DB Engine
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # Fucking SQLite
+)
+
+# Session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

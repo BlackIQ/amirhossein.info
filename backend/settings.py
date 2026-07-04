c47e4a0061dad8e30b92f71b525cebec065b2bc9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 13:10:56 +0330

    Remove unused Supabase variables

--- backend/settings.py
+++ backend/settings.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Settings
 class Settings(BaseSettings):
+    # Application
+    APP_MODE: str = '' # development | production
+    IS_PRODUCTION: bool = APP_MODE == 'production'
+
     # Security
     API_KEY: str = ''
 
@@ -16,19 +20,6 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str = ''
     POSTGRES_HOST: str = ''
     POSTGRES_DATABASE: str = ''
-
-    # SupaBase other env
-    POSTGRES_PRISMA_URL: str = ''
-    POSTGRES_URL_NON_POOLING: str = ''
-    SUPABASE_ANON_KEY: str = ''
-    SUPABASE_URL: str = ''
-    SUPABASE_SERVICE_ROLE_KEY: str = ''
-    SUPABASE_JWT_SECRET: str = ''
-    SUPABASE_PUBLISHABLE_KEY: str = ''
-    SUPABASE_SECRET_KEY: str = ''
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: str = ''
-    NEXT_PUBLIC_SUPABASE_URL: str = ''
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: str = ''
 
     model_config = SettingsConfigDict(env_file=".env")
 

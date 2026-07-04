78d8664503b13bae4db3adda7b248a3561d7240c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 13:11:14 +0330

    Testing another thing in SupaBase POSTGRES_URL

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,3 @@
-# PathLib
-# from pathlib import Path
-
 # SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,21 +5,15 @@ from sqlalchemy.orm import sessionmaker
 # Settings
 from settings import settings
 
-# Path to .db file
-# DB_PATH = Path(__file__).resolve().parent / "app.db"
-# DATABASE_URL = f"sqlite:///{DB_PATH}"
+DATABASE_URL: str = settings.POSTGRES_URL.replace("postgres://", "postgres+psycopg2://")
 
-# DATABASE_URL = f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DATABASE}?sslmode=require&supa=base-pooler.x"
-# DATABASE_URL = f'postgres+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@aws-0-eu-west-1.pooler.supabase.com:6543/{settings.POSTGRES_DATABASE}?sslmode=require&supa=base-pooler.x'
-DATABASE_URL = settings.POSTGRES_URL_NON_POOLING.replace("postgres://", "postgresql+psycopg2://")
-
-print(DATABASE_URL)
+# if settings.IS_PRODUCTION:
+#     DATABASE_URL = settings.POSTGRES_URL_NON_POOLING.replace("postgres://", "postgres+psycopg2://")
+# else:
+#     DATABASE_URL = settings.
 
 # DB Engine
-engine = create_engine(
-    DATABASE_URL,
-    # connect_args={"check_same_thread": False}  # Fucking SQLite
-)
+engine = create_engine(DATABASE_URL)
 
 # Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

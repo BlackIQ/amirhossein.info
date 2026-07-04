2a386f381ea13968d4b44692556cd739c1bf5a26
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 10:01:31 +0330

    Import all models

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from .experience import ExperienceModel
+from .message import MessageModel
+from .note import NoteModel
+from .resume import ResumeModel
+from .skill import SkillModel
+from .social import SocialModel

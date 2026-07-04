f073a20821d053ba4b5f87a4013ddae7c8c644b2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 20:08:04 +0330

    Fixing

--- backend/settings.py
+++ backend/settings.py
@@ -16,26 +16,6 @@ class Settings(BaseSettings):
     # Production PostgreSQL URL
     POSTGRES_PROD_URL: str = ''
 
-    # SupaBase PostgreSQL
-    POSTGRES_URL_NON_POOLING: str = ''
-
-    # SupaBase other env
-    POSTGRES_URL: str = ''
-    POSTGRES_USER: str = ''
-    POSTGRES_PASSWORD: str = ''
-    POSTGRES_HOST: str = ''
-    POSTGRES_DATABASE: str = ''
-    POSTGRES_PRISMA_URL: str = ''
-    SUPABASE_ANON_KEY: str = ''
-    SUPABASE_URL: str = ''
-    SUPABASE_SERVICE_ROLE_KEY: str = ''
-    SUPABASE_JWT_SECRET: str = ''
-    SUPABASE_PUBLISHABLE_KEY: str = ''
-    SUPABASE_SECRET_KEY: str = ''
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: str = ''
-    NEXT_PUBLIC_SUPABASE_URL: str = ''
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: str = ''
-
     model_config = SettingsConfigDict(env_file=".env")
 
 

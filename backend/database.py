a24b43ac819374862458b3173933514f94f0d0c1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-04 14:29:01 +0330

    Testing

--- backend/database.py
+++ backend/database.py
@@ -6,10 +6,15 @@ from sqlalchemy.orm import sessionmaker
 from settings import settings
 
 # Separate Production and Development databases
-if settings.APP_MODE == 'production':
-    DATABASE_URL = settings.POSTGRES_URL_NON_POOLING.replace("postgres://", "postgres+psycopg2://")
-else:
-    DATABASE_URL = settings.POSTGRES_DEV_URL
+# if settings.APP_MODE == 'production':
+#     DATABASE_URL = settings.POSTGRES_URL_NON_POOLING.replace("postgres://", "postgres+psycopg2://")
+# else:
+#     DATABASE_URL = settings.POSTGRES_DEV_URL
+
+DATABASE_URL = settings.POSTGRES_URL_NON_POOLING.replace("postgres://", "postgres+psycopg2://")
+
+# TODO: CHECK DATABASE CONNECTION STRING
+print(DATABASE_URL)
 
 # DB Engine
 engine = create_engine(DATABASE_URL)

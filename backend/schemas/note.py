8289d22d30891495a980c583cd6e7872117eae8b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-05 00:05:02 +0330

    Fix: Return comments in getting single Note

--- backend/schemas/note.py
+++ backend/schemas/note.py
@@ -1,6 +1,9 @@
 # Pydantic
 from pydantic import BaseModel, ConfigDict
 
+# Comment
+from schemas.comment import CommentRead
+
 
 # Schema of a note
 class Note(BaseModel):
@@ -13,5 +16,6 @@ class Note(BaseModel):
 # Schema for id of note
 class NoteRead(Note):
     id: int
+    comments: list[CommentRead] = []
 
     model_config = ConfigDict(from_attributes=True)

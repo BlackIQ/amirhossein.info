0aae60cae938191698a7f1d16b13582ef4081ccb
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 16:25:36 +0330

    Refactor router: Resume

--- backend/routes/resume.py
+++ backend/routes/resume.py
@@ -1,15 +1,13 @@
-# FastAPI, SQLAlchemy
+# FastAPI
 from fastapi import APIRouter, Depends, HTTPException, status
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
 
-# Dependencies
-from dependencies import get_db
-# Middlewares
-from middlewares.apikey import apikey_middleware
-# Models
-from models.resume import ResumeModel
-# Schema
-from schemas.resume import Resume, ResumeRead
+# Application
+from dependencies import apikey, get_db  # Dependencies
+from models import Resume  # Models
+from schemas.resume import ResumeCreate, ResumeRead  # Schemas
 
 # Router
 router = APIRouter(
@@ -18,53 +16,89 @@ router = APIRouter(
 )
 
 
-# Get all Resumes
 @router.get("", response_model=list[ResumeRead])
-async def all_resumes(db: Session = Depends(get_db)):
-    return db.query(ResumeModel).where(ResumeModel.show == True).order_by(ResumeModel.priority).all()
+async def all_resumes(
+    db: Session = Depends(get_db),
+):
+    db_resumes = (
+        db.query(Resume)
+        .where(
+            Resume.show == True,
+        )
+        .order_by(Resume.priority)
+        .all()
+    )
+
+    return db_resumes
 
 
-# Create one Resume
-@router.post("", response_model=ResumeRead, status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(apikey_middleware)])
-async def create_resume(resume: Resume, db: Session = Depends(get_db)):
-    db_item = ResumeModel(**resume.model_dump())
-    db.add(db_item)
+@router.post("", response_model=ResumeRead, status_code=status.HTTP_201_CREATED)
+async def create_resume(
+    resume_data: ResumeCreate,
+    db: Session = Depends(get_db),
+):
+    db_resume = Resume(**resume_data.model_dump())
+
+    db.add(db_resume)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    db.refresh(db_resume)
+
+    return db_resume
 
 
-# Get one Resume
 @router.get("/{resume_id}", response_model=ResumeRead)
-async def get_resume(resume_id: int, db: Session = Depends(get_db)):
-    item = db.get(ResumeModel, resume_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Resume not found")
-    return item
+async def get_resume(
+    resume_id: int,
+    db: Session = Depends(get_db),
+):
+    db_resume = db.get(Resume, resume_id)
+
+    if not db_resume:
+        raise HTTPException(
+            status_code=404,
+            detail="Resume not found",
+        )
+
+    return db_resume
 
 
-# Update one Resume
-@router.put("/{resume_id}", response_model=ResumeRead, dependencies=[Depends(apikey_middleware)])
-async def update_resume(resume_id: int, resume: Resume, db: Session = Depends(get_db)):
-    item = db.get(ResumeModel, resume_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Resume not found")
+@router.put("/{resume_id}", response_model=ResumeRead)
+async def update_resume(
+    resume_id: int,
+    resume_data: ResumeCreate,
+    db: Session = Depends(get_db),
+):
+    db_resume = db.get(Resume, resume_id)
 
-    for key, value in resume.model_dump().items():
-        setattr(item, key, value)
+    if not db_resume:
+        raise HTTPException(
+            status_code=404,
+            detail="Resume not found",
+        )
+
+    for key, value in resume_data.model_dump().items():
+        setattr(db_resume, key, value)
 
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_resume)
+
+    return db_resume
 
 
-# Delete one Resume
-@router.delete("/{resume_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(apikey_middleware)])
-async def delete_resume(resume_id: int, db: Session = Depends(get_db)):
-    item = db.get(ResumeModel, resume_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Resume not found")
-    db.delete(item)
+@router.delete("/{resume_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_resume(
+    resume_id: int,
+    db: Session = Depends(get_db),
+):
+    db_resume = db.get(Resume, resume_id)
+
+    if not db_resume:
+        raise HTTPException(
+            status_code=404,
+            detail="Resume not found",
+        )
+
+    db.delete(db_resume)
     db.commit()
+
     return None

7468f100b264557ae13f110c5857c56733430901
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 16:25:03 +0330

    Refactor router: Experience

--- backend/routes/experience.py
+++ backend/routes/experience.py
@@ -1,15 +1,13 @@
-# FastAPI, SQLAlchemy
+# FastAPI
 from fastapi import APIRouter, Depends, HTTPException, status
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
 
-# Dependencies
-from dependencies import get_db
-# Middlewares
-from middlewares.apikey import apikey_middleware
-# Models
-from models.experience import ExperienceModel
-# Schema
-from schemas.experience import Experience, ExperienceRead
+# Application
+from dependencies import apikey, get_db  # Dependencies
+from models import Experience  # Models
+from schemas.experience import ExperienceRead, ExperienceCreate  # Schemas
 
 # Router
 router = APIRouter(
@@ -18,53 +16,89 @@ router = APIRouter(
 )
 
 
-# Get all Experiences
 @router.get("", response_model=list[ExperienceRead])
-async def all_experiences(db: Session = Depends(get_db)):
-    return db.query(ExperienceModel).where(ExperienceModel.show == True).order_by(ExperienceModel.priority).all()
+async def all_experiences(
+    db: Session = Depends(get_db),
+):
+    db_experiences = (
+        db.query(Experience)
+        .where(
+            Experience.show == True,
+        )
+        .order_by(Experience.priority)
+        .all()
+    )
+
+    return db_experiences
 
 
-# Create one Experience
-@router.post("", response_model=ExperienceRead, status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(apikey_middleware)])
-async def create_experience(experience: Experience, db: Session = Depends(get_db)):
-    db_item = ExperienceModel(**experience.model_dump())
+@router.post("", response_model=ExperienceRead, status_code=status.HTTP_201_CREATED)
+async def create_experience(
+    experience_data: ExperienceCreate,
+    db: Session = Depends(get_db),
+):
+    db_item = Experience(**experience_data.model_dump())
+
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
+
     return db_item
 
 
-# Get one Experience
 @router.get("/{experience_id}", response_model=ExperienceRead)
-async def get_experience(experience_id: int, db: Session = Depends(get_db)):
-    item = db.get(ExperienceModel, experience_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Experience not found")
-    return item
+async def get_experience(
+    experience_id: int,
+    db: Session = Depends(get_db),
+):
+    db_experience = db.get(Experience, experience_id)
+
+    if not db_experience:
+        raise HTTPException(
+            status_code=404,
+            detail="Experience not found",
+        )
+
+    return db_experience
 
 
-# Update one Experience
-@router.put("/{experience_id}", response_model=ExperienceRead, dependencies=[Depends(apikey_middleware)])
-async def update_experience(experience_id: int, experience: Experience, db: Session = Depends(get_db)):
-    item = db.get(ExperienceModel, experience_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Experience not found")
+@router.put("/{experience_id}", response_model=ExperienceRead)
+async def update_experience(
+    experience_id: int,
+    experience_data: ExperienceCreate,
+    db: Session = Depends(get_db),
+):
+    db_experience = db.get(Experience, experience_id)
 
-    for key, value in experience.model_dump().items():
-        setattr(item, key, value)
+    if not db_experience:
+        raise HTTPException(
+            status_code=404,
+            detail="Experience not found",
+        )
+
+    for key, value in experience_data.model_dump().items():
+        setattr(db_experience, key, value)
 
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_experience)
+
+    return db_experience
 
 
-# Delete one Experience
-@router.delete("/{experience_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(apikey_middleware)])
-async def delete_experience(experience_id: int, db: Session = Depends(get_db)):
-    item = db.get(ExperienceModel, experience_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Experience not found")
-    db.delete(item)
+@router.delete("/{experience_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_experience(
+    experience_id: int,
+    db: Session = Depends(get_db),
+):
+    db_experience = db.get(Experience, experience_id)
+
+    if not db_experience:
+        raise HTTPException(
+            status_code=404,
+            detail="Experience not found",
+        )
+
+    db.delete(db_experience)
     db.commit()
+
     return None

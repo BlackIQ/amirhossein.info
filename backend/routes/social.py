cf215334d0d22dec0eb2447f57eed0fedc4a68be
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 16:25:25 +0330

    Refactor router: Social

--- backend/routes/social.py
+++ backend/routes/social.py
@@ -1,15 +1,13 @@
-# FastAPI, SQLAlchemy
+# FastAPI
 from fastapi import APIRouter, Depends, HTTPException, status
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
 
-# Dependencies
-from dependencies import get_db
-# Middlewares
-from middlewares.apikey import apikey_middleware
-# Models
-from models.social import SocialModel
-# Schema
-from schemas.social import Social, SocialRead
+# Application
+from dependencies import apikey, get_db  # Dependencies
+from models import Social  # Models
+from schemas.social import SocialCreate, SocialRead  # Schemas
 
 # Router
 router = APIRouter(
@@ -18,54 +16,89 @@ router = APIRouter(
 )
 
 
-# Get all Socials
 @router.get("", response_model=list[SocialRead])
-async def all_socials(db: Session = Depends(get_db)):
-    return db.query(SocialModel).where(SocialModel.show == True).order_by(SocialModel.priority).all()
+async def all_socials(
+    db: Session = Depends(get_db),
+):
+    db_socials = (
+        db.query(Social)
+        .where(
+            Social.show == True,
+        )
+        .order_by(Social.priority)
+        .all()
+    )
+
+    return db_socials
 
 
-# Create one Social
-@router.post("", response_model=SocialRead, status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(apikey_middleware)])
-async def create_social(social: Social, db: Session = Depends(get_db)):
-    db_item = SocialModel(**social.model_dump())
-    db.add(db_item)
+@router.post("", response_model=SocialRead, status_code=status.HTTP_201_CREATED)
+async def create_social(
+    social_data: SocialCreate,
+    db: Session = Depends(get_db),
+):
+    db_social = Social(**social_data.model_dump())
+
+    db.add(db_social)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    db.refresh(db_social)
+
+    return db_social
 
 
-# Get one Social
 @router.get("/{social_id}", response_model=SocialRead)
-async def get_social(social_id: int, db: Session = Depends(get_db)):
-    item = db.get(SocialModel, social_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Social not found")
-    return item
+async def get_social(
+    social_id: int,
+    db: Session = Depends(get_db),
+):
+    db_social = db.get(Social, social_id)
+
+    if not db_social:
+        raise HTTPException(
+            status_code=404,
+            detail="Social not found",
+        )
+
+    return db_social
 
 
-# Update one Social
-@router.put("/{social_id}", response_model=SocialRead, dependencies=[Depends(apikey_middleware)])
-async def update_social(social_id: int, social: Social, db: Session = Depends(get_db)):
-    item = db.get(SocialModel, social_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Social not found")
+@router.put("/{social_id}", response_model=SocialRead)
+async def update_social(
+    social_id: int,
+    social_data: SocialCreate,
+    db: Session = Depends(get_db),
+):
+    db_social = db.get(Social, social_id)
 
-    for key, value in social.model_dump().items():
-        setattr(item, key, value)
+    if not db_social:
+        raise HTTPException(
+            status_code=404,
+            detail="Social not found",
+        )
+
+    for key, value in social_data.model_dump().items():
+        setattr(db_social, key, value)
 
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_social)
+
+    return db_social
 
 
-# Delete one Social
-@router.delete("/{social_id}", status_code=status.HTTP_204_NO_CONTENT,
-               dependencies=[Depends(apikey_middleware)])
-async def delete_social(social_id: int, db: Session = Depends(get_db)):
-    item = db.get(SocialModel, social_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Social not found")
-    db.delete(item)
+@router.delete("/{social_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_social(
+    social_id: int,
+    db: Session = Depends(get_db),
+):
+    db_social = db.get(Social, social_id)
+
+    if not db_social:
+        raise HTTPException(
+            status_code=404,
+            detail="Social not found",
+        )
+
+    db.delete(db_social)
     db.commit()
+
     return None

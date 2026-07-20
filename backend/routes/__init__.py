b9649ae56638040b54a9cf592796c4dca60e7c39
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 15:33:46 +0330

    We don't need a __init__ file in router

--- backend/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from routes import comment
-from routes import experience
-from routes import message
-from routes import note
-from routes import resume
-from routes import skill
-from routes import social

a4dae179a75bbc7f45a2b2ef9f597b110b897ff4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 15:06:06 +0330

    Deleted files

--- backend/db/base.py
+++
@@ -1,7 +0,0 @@
-# SQLAlchemy DeclarativeBase
-from sqlalchemy.orm import DeclarativeBase
-
-
-# Base class using DeclarativeBase
-class Base(DeclarativeBase):
-    pass
--- backend/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from schemas.comment import Comment, CommentRead
-from schemas.experience import Experience, ExperienceRead
-from schemas.message import Message, MessageRead
-from schemas.note import Note, NoteRead
-from schemas.resume import Resume, ResumeRead
-from schemas.skill import Skill, SkillRead
-from schemas.social import Social, SocialRead

30ef65358f0acb34f517f540d10aeb510c282ce5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-20 16:25:31 +0330

    Refactor router: Note

--- backend/routes/note.py
+++ backend/routes/note.py
@@ -1,15 +1,13 @@
-# FastAPI, SQLAlchemy
+# FastAPI
 from fastapi import APIRouter, Depends, HTTPException, status
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
 
-# Dependencies
-from dependencies import get_db
-# Middlewares
-from middlewares.apikey import apikey_middleware
-# Models
-from models.note import NoteModel
-# Schema
-from schemas.note import Note, NoteRead
+# Application
+from dependencies import apikey, get_db  # Dependencies
+from models import Note  # Models
+from schemas.note import NoteCreate, NoteRead  # Schemas
 
 # Router
 router = APIRouter(
@@ -18,53 +16,88 @@ router = APIRouter(
 )
 
 
-# Get all Notes
 @router.get("", response_model=list[NoteRead])
-async def all_notes(db: Session = Depends(get_db)):
-    return db.query(NoteModel).order_by(NoteModel.id).all()
+async def all_notes(
+    db: Session = Depends(get_db),
+):
+    db_notes = (
+        db.query(Note)
+        .order_by(
+            Note.id.desc(),
+        )
+        .all()
+    )
+
+    return db_notes
 
 
-# Create one Note
-@router.post("", response_model=NoteRead, status_code=status.HTTP_201_CREATED,
-             dependencies=[Depends(apikey_middleware)])
-async def create_note(note: Note, db: Session = Depends(get_db)):
-    db_item = NoteModel(**note.model_dump())
-    db.add(db_item)
+@router.post("", response_model=NoteRead, status_code=status.HTTP_201_CREATED)
+async def create_note(
+    note_data: NoteCreate,
+    db: Session = Depends(get_db),
+):
+    db_note = Note(**note_data.model_dump())
+
+    db.add(db_note)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    db.refresh(db_note)
+
+    return db_note
 
 
-# Get one Note
 @router.get("/{note_id}", response_model=NoteRead)
-async def get_note(note_id: int, db: Session = Depends(get_db)):
-    item = db.get(NoteModel, note_id)
+async def get_note(
+    note_id: int,
+    db: Session = Depends(get_db),
+):
+    item = db.get(Note, note_id)
+
     if not item:
-        raise HTTPException(status_code=404, detail="Note not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Note not found",
+        )
+
     return item
 
 
-# Update one Note
-@router.put("/{note_id}", response_model=NoteRead, dependencies=[Depends(apikey_middleware)])
-async def update_note(note_id: int, note: Note, db: Session = Depends(get_db)):
-    item = db.get(NoteModel, note_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Note not found")
+@router.put("/{note_id}", response_model=NoteRead)
+async def update_note(
+    note_id: int,
+    note_data: NoteCreate,
+    db: Session = Depends(get_db),
+):
+    db_note = db.get(Note, note_id)
 
-    for key, value in note.model_dump().items():
-        setattr(item, key, value)
+    if not db_note:
+        raise HTTPException(
+            status_code=404,
+            detail="Note not found",
+        )
+
+    for key, value in note_data.model_dump().items():
+        setattr(db_note, key, value)
 
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_note)
+
+    return db_note
 
 
-# Delete one Note
-@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(apikey_middleware)])
-async def delete_note(note_id: int, db: Session = Depends(get_db)):
-    item = db.get(NoteModel, note_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Note not found")
-    db.delete(item)
+@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_note(
+    note_id: int,
+    db: Session = Depends(get_db),
+):
+    db_note = db.get(Note, note_id)
+
+    if not db_note:
+        raise HTTPException(
+            status_code=404,
+            detail="Note not found",
+        )
+
+    db.delete(db_note)
     db.commit()
+
     return None

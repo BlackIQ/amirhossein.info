8a4cb168b7b31c72519f0ef131e66a94546830e4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-10 14:52:18 +0330

    Remove some imports and import some new things

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,8 +1,6 @@
 # Models
-from models.comment import Comment
 from models.experience import Experience
 from models.message import Message
-from models.note import Note
 from models.resume import Resume
 from models.skill import Skill
 from models.social import Social

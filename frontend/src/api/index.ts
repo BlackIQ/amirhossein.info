865bd05577df5f293514b7ec6df852d61ed9523a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-24 16:12:38 +0330

    Remove builtin API instance

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,7 +3,3 @@ import axios, { AxiosInstance } from "axios";
 export const API: AxiosInstance = axios.create({
   baseURL: process.env["NEXT_PUBLIC_API_URL"],
 });
-
-export const NextAPI: AxiosInstance = axios.create({
-  baseURL: "/api",
-});

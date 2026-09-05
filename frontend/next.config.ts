8fd90c9165969396ff50059d5e312b8617648ea5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-09-06 00:28:59 +0330

    A test to build a out dir

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
+  output: "export", // To build a out dir
   images: {
+    unoptimized: true,
     domains: ["avatars.githubusercontent.com"],
   },
 };
